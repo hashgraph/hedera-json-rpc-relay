40d17e2cf945bf6fb1a26fd58b00ab44e9108e18
nikolay <n.atanasow94@gmail.com>
2022-04-15 12:45:37 +0300

    Add e2e tests
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- packages/server/tests/server.spec.ts
+++ packages/server/tests/server.spec.ts
@@ -15,7 +15,7 @@ describe('RPC Server', async function() {
   });
 
   after(function() {
-    process.exit(0);
+    this.testServer.close();
   });
 
   it('should execute "eth_chainId"', async function() {
@@ -54,6 +54,235 @@ describe('RPC Server', async function() {
     BaseTest.defaultResponseChecks(res);
     expect(res.data.result).to.be.equal('hashio/' + process.env.npm_package_version);
   });
+
+  it('should execute "eth_getUncleByBlockHashAndIndex"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_getUncleByBlockHashAndIndex',
+      'params': [null]
+    });
+
+    BaseTest.defaultResponseChecks(res);
+    expect(res.data.result).to.be.equal(null);
+  });
+
+  it('should execute "eth_getUncleByBlockNumberAndIndex"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_getUncleByBlockNumberAndIndex',
+      'params': [null]
+    });
+
+    BaseTest.defaultResponseChecks(res);
+    expect(res.data.result).to.be.equal(null);
+  });
+
+  it('should execute "eth_getUncleCountByBlockHash"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_getUncleCountByBlockHash',
+      'params': [null]
+    });
+
+    BaseTest.defaultResponseChecks(res);
+    expect(res.data.result).to.be.equal('0x0');
+  });
+
+  it('should execute "eth_getUncleCountByBlockNumber"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_getUncleCountByBlockNumber',
+      'params': [null]
+    });
+
+    BaseTest.defaultResponseChecks(res);
+    expect(res.data.result).to.be.equal('0x0');
+  });
+
+  it('should execute "eth_hashrate"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_hashrate',
+      'params': [null]
+    });
+
+    BaseTest.defaultResponseChecks(res);
+    expect(res.data.result).to.be.equal('0x0');
+  });
+
+  it('should execute "eth_mining"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_mining',
+      'params': [null]
+    });
+
+    BaseTest.defaultResponseChecks(res);
+    expect(res.data.result).to.be.equal(false);
+  });
+
+  it('should execute "eth_submitWork"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_submitWork',
+      'params': [null]
+    });
+
+    BaseTest.defaultResponseChecks(res);
+    expect(res.data.result).to.be.equal(false);
+  });
+
+  it('should execute "eth_syncing"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_syncing',
+      'params': [null]
+    });
+
+    BaseTest.defaultResponseChecks(res);
+    expect(res.data.result).to.be.equal(false);
+  });
+
+  it('should execute "net_listening"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'net_listening',
+      'params': [null]
+    });
+
+    BaseTest.defaultResponseChecks(res);
+    expect(res.data.result).to.be.equal('false');
+  });
+
+  it('should execute "web3_sha"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'web3_sha',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
+
+  it('should execute "parity_nextNonce"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'parity_nextNonce',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
+
+  it('should execute "net_peerCount"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'net_peerCount',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
+
+  it('should execute "eth_submitHashrate"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_submitHashrate',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
+
+  it('should execute "eth_signTypedData"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_signTypedData',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
+
+  it('should execute "eth_signTransaction"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_signTransaction',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
+
+  it('should execute "eth_sign"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_sign',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
+
+  it('should execute "eth_sendTransaction"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_sendTransaction',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
+
+  it('should execute "eth_protocolVersion"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_protocolVersion',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
+
+  it('should execute "eth_getProof"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_getProof',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
+
+  it('should execute "eth_coinbase"', async function() {
+    const res = await this.testClient.post('/', {
+      'id': '2',
+      'jsonrpc': '2.0',
+      'method': 'eth_coinbase',
+      'params': [null]
+    });
+
+    BaseTest.unsupportedJsonRpcMethodChecks(res);
+  });
 });
 
 class BaseTest {
@@ -76,5 +305,18 @@ class BaseTest {
     expect(response.data).to.have.property('result');
     expect(response.data.id).to.be.equal('2');
     expect(response.data.jsonrpc).to.be.equal('2.0');
+  }
+
+  static unsupportedJsonRpcMethodChecks(response) {
+    expect(response).to.have.property('data');
+    expect(response.data).to.have.property('id');
+    expect(response.data).to.have.property('jsonrpc');
+    expect(response.data).to.have.property('error');
+    expect(response.data.id).to.be.equal('2');
+    expect(response.data.jsonrpc).to.be.equal('2.0');
+    expect(response.data.error).to.have.property('message');
+    expect(response.data.error).to.have.property('code');
+    expect(response.data.error.message).to.be.equal('Method not found');
+    expect(response.data.error.code).to.be.equal(-32601);
   }
 }
