1c359ab91cddc1d156f830bd7061101f5194952d
Nana-EC <nana@swirldslabs.com>
2022-05-11 15:20:14 -0500

    Add License headers to all code files
    Signed-off-by: Nana-EC <nana@swirldslabs.com>

--- packages/relay/src/lib/mirrorNode.ts
+++ packages/relay/src/lib/mirrorNode.ts
@@ -1,6 +1,23 @@
-import Axios, { AxiosInstance } from 'axios';
-import Keyv from 'keyv';
-import { KeyvFile } from 'keyv-file';
+/*-
+ *
+ * Hedera JSON RPC Relay
+ *
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 import { predefined as errors } from './errors';
 import {Logger} from "pino";
 // Used for temporary purposes to store block info. As the mirror node supports the APIs, we will remove this.
