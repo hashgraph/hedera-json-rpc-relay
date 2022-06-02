9a72f9a798c4eb4a3c3d97782996bc3906603483
Nana-EC <nana@swirldslabs.com>
2022-06-01 19:15:14 -0500

    Add sendRawTransaction tests
    Signed-off-by: Nana-EC <nana@swirldslabs.com>

--- packages/server/tests/acceptance.spec.ts
+++ packages/server/tests/acceptance.spec.ts
@@ -30,10 +30,11 @@ import {
     ContractCreateTransaction,
     ContractExecuteTransaction,
     ContractFunctionParameters,
-    EthereumTransaction,
     FileCreateTransaction,
+    Query,
     TokenAssociateTransaction,
     TokenCreateTransaction,
+    Transaction,
     TransactionResponse,
     TransferTransaction,
 } from "@hashgraph/sdk";
@@ -49,21 +50,27 @@ import parentContract from './parentContract/Parent.json';
 import app from '../src/server';
 
 const testLogger = pino({
-  name: 'hedera-json-rpc-relay',
-  level: process.env.LOG_LEVEL || 'trace',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: true
+    name: 'hedera-json-rpc-relay',
+    level: process.env.LOG_LEVEL || 'trace',
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            translateTime: true
+        }
     }
-  }
 });
 const logger = testLogger.child({ name: 'rpc-acceptance-test' });
 
 
 dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 const useLocalNode = process.env.LOCAL_NODE || 'true';
+
+// const refs
+const legacyTransactionHex = 'f864012f83018000947e3a9eaf9bcc39e2ffa38eb30bf7a93feacbc18180827653820277a0f9fbff985d374be4a55f296915002eec11ac96f1ce2df183adf992baa9390b2fa00c1e867cc960d9c74ec2e6a662b7908ec4c8cc9f3091e886bcefbeb2290fb792';
+const eip155TransactionHex = 'f86c098504a817c800825208943535353535353535353535353535353535353535880de0b6b3a76400008025a028ef61340bd939bc2195fe537567866003e1a15d3c71ff63e1590620aa636276a067cbe9d8997f761aecb703304b3800ccf555c9f3dc64214b297fb1966a3b6d83';
+const londonTransactionHex = '02f902e082012a80a00000000000000000000000000000000000000000000000000000000000004e20a0000000000000000000000000000000000000000000000000000000746a528800830f42408080b9024d608060405261023a806100136000396000f3fe60806040526004361061003f5760003560e01c806312065fe01461008f5780633ccfd60b146100ba5780636f64234e146100d1578063b6b55f251461012c575b3373ffffffffffffffffffffffffffffffffffffffff167ff1b03f708b9c39f453fe3f0cef84164c7d6f7df836df0796e1e9c2bce6ee397e346040518082815260200191505060405180910390a2005b34801561009b57600080fd5b506100a461015a565b6040518082815260200191505060405180910390f35b3480156100c657600080fd5b506100cf610162565b005b3480156100dd57600080fd5b5061012a600480360360408110156100f457600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190803590602001909291905050506101ab565b005b6101586004803603602081101561014257600080fd5b81019080803590602001909291905050506101f6565b005b600047905090565b3373ffffffffffffffffffffffffffffffffffffffff166108fc479081150290604051600060405180830381858888f193505050501580156101a8573d6000803e3d6000fd5b50565b8173ffffffffffffffffffffffffffffffffffffffff166108fc829081150290604051600060405180830381858888f193505050501580156101f1573d6000803e3d6000fd5b505050565b80341461020257600080fd5b5056fea265627a7a72315820f8f84fc31a845064b5781e908316f3c591157962deabb0fd424ed54f256400f964736f6c63430005110032c001a01f7e8e436e6035ef7e5cd1387e2ad679e74d6a78a2736efe3dee72e531e28505a042b40a9cf56aad4530a5beaa8623f1ac3554d59ac1e927c672287eb45bfe7b8d';
+
 
 // cached entities
 let client: Client;
@@ -81,12 +88,12 @@ describe('RPC Server Integration Tests', async function () {
     this.timeout(180 * 1000);
 
     before(async function () {
-        logger.info('Setting up SDK Client for local instances');
+        logger.info(`Setting up SDK Client for ${process.env['HEDERA_NETWORK']} env`);
         setupClient();
 
         if (useLocalNode === 'true') {
             // set env variables for docker images until local-node is updated
-            process.env['NETWORK_NODE_IMAGE_TAG'] = '0.25.4';
+            process.env['NETWORK_NODE_IMAGE_TAG'] = '0.26.2-patch.3';
             process.env['HAVEGED_IMAGE_TAG'] = '0.25.4';
             process.env['MIRROR_IMAGE_TAG'] = '0.57.2';
             logger.trace(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
@@ -111,17 +118,13 @@ describe('RPC Server Integration Tests', async function () {
         await executeContractCall();
 
         logger.info('Create token');
-        // 2. Token create
+        // 3. Token create
         await createToken();
 
         logger.info('Associate and transfer tokens');
         // 4. associate and transfer 2 tokens
         await associateAndTransferToken(primaryAccountInfo.accountId, primaryKey);
         await associateAndTransferToken(secondaryAccountInfo.accountId, secondaryKey);
-
-        // requires 0.26.0 of service nodes
-        // logger.info('Submit ethereum transaction types');
-        // await submitEthereumTransactions();
 
         logger.info('Send file close crypto transfers');
         // 5. simple crypto trasnfer to ensure file close
@@ -322,6 +325,24 @@ describe('RPC Server Integration Tests', async function () {
         expect(res.data.result).to.be.equal(false);
     });
 
+    it('should execute eth_sendRawTransaction legacy', async function () {
+        const res = await callSupportedRelayMethod(this.relayClient, 'eth_sendRawTransaction', ['0x' + legacyTransactionHex]);
+        logger.info(`*** eth_sendRawTransaction" legacy res.data.result: ${res.data.result}`);
+        expect(res.data.result).to.be.equal('0x9ffbd69c44cf643ed8d1e756b505e545e3b5dd3a6b5ef9da1d8eca6679706594');
+    });
+
+    // failing with WRONG_CHAIN_ID
+    // it('should execute "eth_sendRawTransaction"  eip155', async function () {
+    //     const res = await callSupportedRelayMethod(this.relayClient, 'eth_sendRawTransaction', ['0x' + eip155TransactionHex]);
+    //     logger.info(`*** eth_sendRawTransaction" eip155 res.data.result: ${res.data.result}`);
+    //     expect(res.data.result).to.be.equal(false);
+    // });
+
+    it('should execute "eth_sendRawTransaction" london', async function () {
+        const res = await callSupportedRelayMethod(this.relayClient, 'eth_sendRawTransaction', ['0x' + londonTransactionHex]);
+        expect(res.data.result).to.be.equal('0xcdbbfb6400aab319f97d32c38e285f0d0399c2b48b683f04878b5f07eb0d50e3');
+    });
+
     it('should execute "eth_syncing"', async function () {
         const res = await callSupportedRelayMethod(this.relayClient, 'eth_syncing', []);
         expect(res.data.result).to.be.equal(false);
@@ -398,107 +419,67 @@ describe('RPC Server Integration Tests', async function () {
         logger.trace(`New Eth compatible privateKey: ${privateKey}`);
         logger.trace(`New Eth compatible publicKey: ${publicKey}`);
         logger.debug(`New Eth compatible account ID: ${aliasAccountId.toString()}`);
-        const aliasCreationResponse = await new TransferTransaction()
+
+        logger.info(`Transfer transaction attempt`);
+        const aliasCreationResponse = await executeTransaction(new TransferTransaction()
             .addHbarTransfer(client.operatorAccountId, new Hbar(100).negated())
-            .addHbarTransfer(aliasAccountId, new Hbar(100))
-            .execute(client);
+            .addHbarTransfer(aliasAccountId, new Hbar(100)));
 
         logger.debug(`Get ${aliasAccountId.toString()} receipt`);
         await aliasCreationResponse.getReceipt(client);
 
-        const balance = await new AccountBalanceQuery()
+        const balance = await executeQuery(new AccountBalanceQuery()
             .setNodeAccountIds([aliasCreationResponse.nodeId])
-            .setAccountId(aliasAccountId)
-            .execute(client);
+            .setAccountId(aliasAccountId));
 
         logger.info(`Balances of the new account: ${balance.toString()}`);
 
-        const accountInfo = await new AccountInfoQuery()
+        const accountInfo = await executeQuery(new AccountInfoQuery()
             .setNodeAccountIds([aliasCreationResponse.nodeId])
-            .setAccountId(aliasAccountId)
-            .execute(client);
+            .setAccountId(aliasAccountId));
 
         logger.info(`New account Info: ${accountInfo.accountId.toString()}`);
         return { accountInfo, privateKey };
     };
 
-    const submitEthereumTransactions = async () => {
-        // Convert from 0xabc format into a raw Uint8Array of bytes and execute the transaction
-        const legacyTransactionBuffer = Buffer.from('f864012f83018000947e3a9eaf9bcc39e2ffa38eb30bf7a93feacbc18180827653820277a0f9fbff985d374be4a55f296915002eec11ac96f1ce2df183adf992baa9390b2fa00c1e867cc960d9c74ec2e6a662b7908ec4c8cc9f3091e886bcefbeb2290fb792', 'hex');
-        const eip155TransactionBuffer = Buffer.from('f86c098504a817c800825208943535353535353535353535353535353535353535880de0b6b3a76400008025a028ef61340bd939bc2195fe537567866003e1a15d3c71ff63e1590620aa636276a067cbe9d8997f761aecb703304b3800ccf555c9f3dc64214b297fb1966a3b6d83', 'hex');
-        const londonTransactionBuffer = Buffer.from('02f902e082012a80a00000000000000000000000000000000000000000000000000000000000004e20a0000000000000000000000000000000000000000000000000000000746a528800830f42408080b9024d608060405261023a806100136000396000f3fe60806040526004361061003f5760003560e01c806312065fe01461008f5780633ccfd60b146100ba5780636f64234e146100d1578063b6b55f251461012c575b3373ffffffffffffffffffffffffffffffffffffffff167ff1b03f708b9c39f453fe3f0cef84164c7d6f7df836df0796e1e9c2bce6ee397e346040518082815260200191505060405180910390a2005b34801561009b57600080fd5b506100a461015a565b6040518082815260200191505060405180910390f35b3480156100c657600080fd5b506100cf610162565b005b3480156100dd57600080fd5b5061012a600480360360408110156100f457600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190803590602001909291905050506101ab565b005b6101586004803603602081101561014257600080fd5b81019080803590602001909291905050506101f6565b005b600047905090565b3373ffffffffffffffffffffffffffffffffffffffff166108fc479081150290604051600060405180830381858888f193505050501580156101a8573d6000803e3d6000fd5b50565b8173ffffffffffffffffffffffffffffffffffffffff166108fc829081150290604051600060405180830381858888f193505050501580156101f1573d6000803e3d6000fd5b505050565b80341461020257600080fd5b5056fea265627a7a72315820f8f84fc31a845064b5781e908316f3c591157962deabb0fd424ed54f256400f964736f6c63430005110032c001a01f7e8e436e6035ef7e5cd1387e2ad679e74d6a78a2736efe3dee72e531e28505a042b40a9cf56aad4530a5beaa8623f1ac3554d59ac1e927c672287eb45bfe7b8d', 'hex');
-
-        // legacy
-        logger.info(`Submit legacy type ethereum transaction`);
-        const legacyResp = await new EthereumTransaction()
-            .setEthereumData(legacyTransactionBuffer)
-            .execute(client);
-
-        const { executedTimestamp: legacyTimestamp, executedTransactionId: legacyTransactionId } = await getRecordResponseDetails(legacyResp);
-
-        // eip 155
-        logger.info(`Submit eip 155 type ethereum transaction`);
-        const eip155Resp = await new EthereumTransaction()
-            .setEthereumData(eip155TransactionBuffer)
-            .execute(client);
-
-        const { executedTimestamp: eip155Timestamp, executedTransactionId: eip155TransactionId } = await getRecordResponseDetails(eip155Resp);
-
-        // eip 1559
-        logger.info(`Submit eip 1559 type ethereum transaction`);
-        const eip1559Resp = await new EthereumTransaction()
-            .setEthereumData(londonTransactionBuffer)
-            .execute(client);
-
-        const { executedTimestamp: eip1559Timestamp, executedTransactionId: eip1559TransactionId } = await getRecordResponseDetails(eip1559Resp);
-    };
-
     const createToken = async () => {
         const symbol = Math.random().toString(36).slice(2, 6).toUpperCase();
         logger.trace(`symbol = ${symbol}`);
-        const resp = await new TokenCreateTransaction()
+        const resp = await executeAndGetTransactionReceipt(new TokenCreateTransaction()
             .setTokenName(`relay-acceptance token ${symbol}`)
             .setTokenSymbol(symbol)
             .setDecimals(3)
             .setInitialSupply(1000)
-            .setTreasuryAccountId(client.operatorAccountId)
-            .execute(client);
+            .setTreasuryAccountId(client.operatorAccountId));
 
         logger.trace(`get token id from receipt`);
-        tokenId = (await resp.getReceipt(client)).tokenId;
+        tokenId = resp.tokenId;
         logger.info(`token id = ${tokenId.toString()}`);
     };
 
     const associateAndTransferToken = async (accountId: AccountId, pk: PrivateKey) => {
         logger.info(`Associate account ${accountId.toString()} with token ${tokenId.toString()}`);
-        await (
-            await (
-                await new TokenAssociateTransaction()
-                    .setAccountId(accountId)
-                    .setTokenIds([tokenId])
-                    .freezeWith(client)
-                    .sign(pk)
-            ).execute(client)
-        ).getReceipt(client);
+        await executeAndGetTransactionReceipt(
+            await new TokenAssociateTransaction()
+                .setAccountId(accountId)
+                .setTokenIds([tokenId])
+                .freezeWith(client)
+                .sign(pk));
 
         logger.debug(
             `Associated account ${accountId.toString()} with token ${tokenId.toString()}`
         );
 
-        await (
-            await new TransferTransaction()
-                .addTokenTransfer(tokenId, client.operatorAccountId, -10)
-                .addTokenTransfer(tokenId, accountId, 10)
-                .execute(client)
-        ).getReceipt(client);
+        executeAndGetTransactionReceipt(new TransferTransaction()
+            .addTokenTransfer(tokenId, client.operatorAccountId, -10)
+            .addTokenTransfer(tokenId, accountId, 10));
 
         logger.debug(
             `Sent 10 tokens from account ${client.operatorAccountId.toString()} to account ${accountId.toString()} on token ${tokenId.toString()}`
         );
 
-        const balances = await new AccountBalanceQuery()
-            .setAccountId(accountId)
-            .execute(client);
+        const balances = await executeQuery(new AccountBalanceQuery()
+            .setAccountId(accountId));
 
         logger.debug(
             `Token balances for ${accountId.toString()} are ${balances.tokens
@@ -508,17 +489,15 @@ describe('RPC Server Integration Tests', async function () {
     };
 
     const sendFileClosingCryptoTransfer = async (accountId: AccountId) => {
-        const aliasCreationResponse = await new TransferTransaction()
+        const aliasCreationResponse = await executeTransaction(new TransferTransaction()
             .addHbarTransfer(client.operatorAccountId, new Hbar(1, HbarUnit.Millibar).negated())
-            .addHbarTransfer(accountId, new Hbar(1, HbarUnit.Millibar))
-            .execute(client);
+            .addHbarTransfer(accountId, new Hbar(1, HbarUnit.Millibar)));
 
         await aliasCreationResponse.getReceipt(client);
 
-        const balance = await new AccountBalanceQuery()
+        const balance = await executeQuery(new AccountBalanceQuery()
             .setNodeAccountIds([aliasCreationResponse.nodeId])
-            .setAccountId(accountId)
-            .execute(client);
+            .setAccountId(accountId));
 
         logger.info(`Balances of the new account: ${balance.toString()}`);
     };
@@ -526,60 +505,86 @@ describe('RPC Server Integration Tests', async function () {
     const createParentContract = async () => {
         const contractByteCode = (parentContract.deployedBytecode.replace('0x', ''));
 
-        const fileTransactionResponse = await new FileCreateTransaction()
+        const fileReceipt = await executeAndGetTransactionReceipt(new FileCreateTransaction()
             .setKeys([client.operatorPublicKey])
-            .setContents(contractByteCode)
-            .execute(client);
+            .setContents(contractByteCode));
 
         // Fetch the receipt for transaction that created the file
-        const fileReceipt = await fileTransactionResponse.getReceipt(client);
-
         // The file ID is located on the transaction receipt
         const fileId = fileReceipt.fileId;
         logger.info(`contract bytecode file: ${fileId.toString()}`);
 
         // Create the contract
-        const contractTransactionResponse = await new ContractCreateTransaction()
+        const contractReceipt = await executeAndGetTransactionReceipt(new ContractCreateTransaction()
             .setConstructorParameters(
                 new ContractFunctionParameters()
             )
             .setGas(75000)
             .setBytecodeFileId(fileId)
-            .setAdminKey(client.operatorPublicKey)
-            .execute(client);
+            .setAdminKey(client.operatorPublicKey));
 
         // Fetch the receipt for the transaction that created the contract
-        const contractReceipt = await contractTransactionResponse.getReceipt(
-            client
-        );
 
         // The conract ID is located on the transaction receipt
         contractId = contractReceipt.contractId;
 
         logger.info(`new contract ID: ${contractId.toString()}`);
-        await getRecordResponseDetails(contractTransactionResponse);
     };
 
     const executeContractCall = async () => {
         // Call a method on a contract exists on Hedera, but is allowed to mutate the contract state
         logger.info(`Execute contracts ${contractId}'s createChild method`);
         const contractExecTransactionResponse =
-            await new ContractExecuteTransaction()
+            await executeTransaction(new ContractExecuteTransaction()
                 .setContractId(contractId)
                 .setGas(75000)
                 .setFunction(
                     "createChild",
                     new ContractFunctionParameters()
                         .addUint256(1000)
-                )
-                .execute(client);
+                ));
 
         const resp = await getRecordResponseDetails(contractExecTransactionResponse);
         contractExecuteTimestamp = resp.executedTimestamp;
         contractExecutedTransactionId = resp.executedTransactionId;
     };
 
+    const executeQuery = async (query: Query<any>) => {
+        try {
+            logger.info(`Execute ${query.constructor.name} query`);
+            return query.execute(client);
+        }
+        catch (e) {
+            logger.error(e, `Error executing ${query.constructor.name} query`);
+        }
+    };
+
+    const executeTransaction = async (transaction: Transaction) => {
+        try {
+            logger.info(`Execute ${transaction.constructor.name} transaction`);
+            const resp = await transaction.execute(client);
+            logger.info(`Executed transaction ${resp.transactionId.toString()}`);
+            return resp;
+        }
+        catch (e) {
+            logger.error(e, `Error executing ${transaction.constructor.name} transaction`);
+        }
+    };
+
+    const executeAndGetTransactionReceipt = async (transaction: Transaction) => {
+        let resp;
+        try {
+            resp = await executeTransaction(transaction);
+            return resp.getReceipt(client);
+        }
+        catch (e) {
+            logger.error(e, 
+                `Error retrieving receipt for ${resp === undefined ? transaction.constructor.name : resp.transactionId.toString()} transaction`);
+        }
+    };
+
     const getRecordResponseDetails = async (resp: TransactionResponse) => {
+        logger.info(`Retrieve record for ${resp.transactionId.toString()}`);
         const record = await resp.getRecord(client);
         const nanoString = record.consensusTimestamp.nanos.toString();
         const executedTimestamp = `${record.consensusTimestamp.seconds}.${nanoString.padStart(9, '0')}`;
