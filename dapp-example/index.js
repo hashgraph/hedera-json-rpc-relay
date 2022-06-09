5aa01cd4d510278446c2305414085cd11b672580
nikolay <n.atanasow94@gmail.com>
2022-06-09 09:36:42 +0300

    Add base structure
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

---
+++ dapp-example/index.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3001
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.htm'));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
