61908dae163a5800b0332003eda771db8474f232
nikolay <n.atanasow94@gmail.com>
2022-06-17 17:17:59 +0300

    Add prerequisite script
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

---
+++ packages/server/tests/helpers/prerequisite.ts
@@ -0,0 +1,21 @@
+import app from '../src/server';
+import shell from 'shelljs';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+
+(function () {
+  process.env['NETWORK_NODE_IMAGE_TAG'] = '0.26.2';
+  process.env['HAVEGED_IMAGE_TAG'] = '0.26.2';
+  process.env['MIRROR_IMAGE_TAG'] = '0.58.0';
+  console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
+
+  // start local-node
+  console.log('Start local node');
+  shell.exec('npx hedera-local restart');
+  console.log('Hedera Hashgraph local node env started');
+
+  console.log(`Start relay on port ${process.env.SERVER_PORT}`);
+  shell.exec('npm run start');
+})();
