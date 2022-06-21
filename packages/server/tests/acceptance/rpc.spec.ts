3cf27aa3de935fe42f948c28deaa4dc77ed09f6e
Nana-EC <nana@swirldslabs.com>
2022-06-21 17:18:53 -0500

    Move get end balance up
    Signed-off-by: Nana-EC <nana@swirldslabs.com>

--- packages/server/tests/acceptance/rpc.spec.ts
+++ packages/server/tests/acceptance/rpc.spec.ts
@@ -668,6 +668,10 @@ describe('RPC Server Acceptance Tests', function () {
     });
 
     this.afterAll(async () =>  {
+        const endOperatorBalance = await servicesNode.getOperatorBalance();
+        const cost = startOperatorBalance.toTinybars().subtract(endOperatorBalance.toTinybars());
+        logger.info(`Acceptance Tests spent ${Hbar.fromTinybars(cost)}`);
+
         if (USE_LOCAL_NODE === 'true') {
             // stop local-node
             logger.info('Shutdown local node');
@@ -679,10 +683,6 @@ describe('RPC Server Acceptance Tests', function () {
         if (relayServer !== undefined) {
             relayServer.close();
         }
-
-        const endOperatorBalance = await servicesNode.getOperatorBalance();
-        const cost = startOperatorBalance.toTinybars().subtract(endOperatorBalance.toTinybars());
-        logger.info(`Acceptance Tests spent ${Hbar.fromTinybars(cost)}`);
     });
 
     function runLocalHederaNetwork() {
