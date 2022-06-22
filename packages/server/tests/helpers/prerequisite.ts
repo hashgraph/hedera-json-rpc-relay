2ae4e2ea8f24c553e13eac3ff2808f6c17873965
Anton Rusev <anton.rusev.code@gmail.com>
2022-06-22 09:09:59 +0300

    test the ci
    Signed-off-by: Anton Rusev <anton.rusev.code@gmail.com>

--- packages/server/tests/helpers/prerequisite.ts
+++ packages/server/tests/helpers/prerequisite.ts
@@ -26,6 +26,4 @@ const RELAY_URL = process.env.E2E_RELAY_HOST || LOCAL_RELAY_URL;
     console.log(`Start relay on port ${process.env.SERVER_PORT}`);
     app.listen({ port: process.env.SERVER_PORT });
   }
-
-  shell.exec(`cd ${process.cwd()} && xvfb-run -a --server-args="-screen 0 1280x1024x24" npm run only-tests --prefix ./dapp-example/`)
 })();
