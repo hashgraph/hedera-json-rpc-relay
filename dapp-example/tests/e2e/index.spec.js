1d9050a844da74b04dd716cd78ca14c5b65f73dc
Anton Rusev <anton.rusev.code@gmail.com>
2022-06-22 12:01:06 +0300

    test the ci
    Signed-off-by: Anton Rusev <anton.rusev.code@gmail.com>

--- dapp-example/tests/e2e/index.spec.js
+++ dapp-example/tests/e2e/index.spec.js
@@ -1,10 +1,10 @@
 import 'cypress-wait-until';
 import * as htsData from '../../.htsTokenAddress.json';
 
-describe('Test Core Hedera User Scenarios', function() {
+describe('Test Core Hedera User Scenarios', function () {
   this.timeout(180000);
 
-  const connectToMetamask = function() {
+  const connectToMetamask = function () {
     cy.visit('http://localhost:3000');
     cy.contains('Connect Account').click();
     cy.switchToMetamaskWindow();
@@ -12,7 +12,7 @@ describe('Test Core Hedera User Scenarios', function() {
     cy.switchToCypressWindow();
   };
 
-  it('Connects with Metamask', function() {
+  it('Connects with Metamask', function () {
     connectToMetamask();
 
     // check the UI
@@ -22,20 +22,26 @@ describe('Test Core Hedera User Scenarios', function() {
     cy.get('#btnUpdateGreeting').should('not.be.disabled');
   }).timeout(180000);
 
-  it('Show alias', function() {
+  it('Show alias', function () {
     // connectToMetamask();
     cy.visit('http://localhost:3000');
     cy.contains('Connect Account').click();
+    cy.switchToMetamaskWindow();
+    cy.acceptMetamaskAccess();
+    cy.switchToCypressWindow();
 
     cy.get('#showAliasBtn').should('not.be.disabled').click();
     cy.confirmMetamaskSignatureRequest();
     cy.waitUntil(() => cy.get('#aliasField').invoke('text').should('have.length', 66));
   }).timeout(180000);
 
-  it('Deploy contract', function() {
+  it('Deploy contract', function () {
     // connectToMetamask();
     cy.visit('http://localhost:3000');
     cy.contains('Connect Account').click();
+    cy.switchToMetamaskWindow();
+    cy.acceptMetamaskAccess();
+    cy.switchToCypressWindow();
 
     // deploy the contract
     cy.get('#btnDeployContract').should('not.be.disabled').click();
@@ -57,10 +63,13 @@ describe('Test Core Hedera User Scenarios', function() {
     cy.waitUntil(() => cy.get('#contractViewMsg').should('have.text', ' Result: updated_text '));
   }).timeout(180000);
 
-  it('Transfer HTS token', function() {
+  it('Transfer HTS token', function () {
     // connectToMetamask();
     cy.visit('http://localhost:3000');
     cy.contains('Connect Account').click();
+    cy.switchToMetamaskWindow();
+    cy.acceptMetamaskAccess();
+    cy.switchToCypressWindow();
 
     // test the HTS transfer
     cy.get('#htsTokenAddressField').type(htsData.HTS_ADDRESS);
@@ -72,10 +81,13 @@ describe('Test Core Hedera User Scenarios', function() {
     cy.waitUntil(() => cy.get('#htsTokenMsg').should('have.text', ' Done '));
   }).timeout(180000);
 
-  it('Transfer HBARs', function() {
+  it('Transfer HBARs', function () {
     // connectToMetamask();
     cy.visit('http://localhost:3000');
     cy.contains('Connect Account').click();
+    cy.switchToMetamaskWindow();
+    cy.acceptMetamaskAccess();
+    cy.switchToCypressWindow();
 
     cy.get('#sendHbarsToField').type('0x54C51b7637BF6fE9709e1e0EBc8b2Ca6a24b0f0A');
     cy.get('#sendHbarsAmountField').type('10000000000000000');
