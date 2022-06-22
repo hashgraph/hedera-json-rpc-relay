b4251d146bbe9d092bd45823c3fc7e1093daa76e
Anton Rusev <anton.rusev.code@gmail.com>
2022-06-22 08:59:37 +0300

    test the ci
    Signed-off-by: Anton Rusev <anton.rusev.code@gmail.com>

--- packages/server/tests/helpers/prerequisite.ts
+++ packages/server/tests/helpers/prerequisite.ts
@@ -10,20 +10,22 @@ const LOCAL_RELAY_URL = 'http://localhost:7546';
 const RELAY_URL = process.env.E2E_RELAY_HOST || LOCAL_RELAY_URL;
 
 (function () {
-  if (USE_LOCAL_NODE) {
-    process.env['NETWORK_NODE_IMAGE_TAG'] = '0.26.2';
-    process.env['HAVEGED_IMAGE_TAG'] = '0.26.2';
-    process.env['MIRROR_IMAGE_TAG'] = '0.58.0';
-    console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
+  // if (USE_LOCAL_NODE) {
+  //   process.env['NETWORK_NODE_IMAGE_TAG'] = '0.26.2';
+  //   process.env['HAVEGED_IMAGE_TAG'] = '0.26.2';
+  //   process.env['MIRROR_IMAGE_TAG'] = '0.58.0';
+  //   console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
 
-    // start local-node
-    console.log('Start local node');
-    shell.exec('npx hedera-local restart');
-    console.log('Hedera Hashgraph local node env started');
-  }
+  //   // start local-node
+  //   console.log('Start local node');
+  //   shell.exec('npx hedera-local restart');
+  //   console.log('Hedera Hashgraph local node env started');
+  // }
 
   if (RELAY_URL === LOCAL_RELAY_URL) {
     console.log(`Start relay on port ${process.env.SERVER_PORT}`);
-    shell.exec('npm run start');
+    app.listen({ port: process.env.SERVER_PORT });
   }
+
+  shell.exec(`cd ${process.cwd()} && xvfb-run -a --server-args="-screen 0 1280x1024x24" npm run only-tests --prefix ./dapp-example/`)
 })();
