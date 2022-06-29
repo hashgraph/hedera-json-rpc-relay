fbe76eb3b2ebdbad2fa85d05970d1dbcd4c41fd3
Anton Rusev <anton.rusev.code@gmail.com>
2022-06-29 15:32:19 +0300

    test: revert mirror image version
    Signed-off-by: Anton Rusev <anton.rusev.code@gmail.com>

--- packages/server/tests/acceptance/rpc.spec.ts
+++ packages/server/tests/acceptance/rpc.spec.ts
@@ -718,7 +718,7 @@ describe('RPC Server Acceptance Tests', function () {
         // set env variables for docker images until local-node is updated
         process.env['NETWORK_NODE_IMAGE_TAG'] = '0.26.3';
         process.env['HAVEGED_IMAGE_TAG'] = '0.26.3';
-        process.env['MIRROR_IMAGE_TAG'] = '0.59.0-rc1';
+        process.env['MIRROR_IMAGE_TAG'] = '0.58.0';
         logger.trace(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
 
         // start local-node
