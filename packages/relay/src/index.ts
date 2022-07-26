79716e4c72823db6e49f7565ab21e540112ff105
lukelee-sl <luke.lee@swirldslabs.com>
2022-07-26 15:10:05 -0700

    fix build error
    Signed-off-by: lukelee-sl <luke.lee@swirldslabs.com>

--- packages/relay/src/index.ts
+++ packages/relay/src/index.ts
@@ -79,7 +79,7 @@ export interface Eth {
 
   getTransactionByHash(hash: string): Promise<Transaction | null>;
   
-  getTransactionCount(address: string, blocknum: string): Promise<string>;
+  getTransactionCount(address: string, blocknum: string): Promise<string | JsonRpcError>;
 
   getTransactionReceipt(hash: string): Promise<Receipt | null>;
 
