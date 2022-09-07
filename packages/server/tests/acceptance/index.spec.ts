6b269a2bbff003d71fa9393e09d53ceaf7b2cd72
Nana Essilfie-Conduah <nana@swirldslabs.com>
2022-09-06 19:02:15 -0500

    Bump mirror node to 0.64.0-rc1
    Signed-off-by: Nana Essilfie-Conduah <nana@swirldslabs.com>

--- packages/server/tests/acceptance/index.spec.ts
+++ packages/server/tests/acceptance/index.spec.ts
@@ -118,7 +118,7 @@ describe('RPC Server Acceptance Tests', function () {
         // set env variables for docker images until local-node is updated
         process.env['NETWORK_NODE_IMAGE_TAG'] = '0.30.0-alpha.0';
         process.env['HAVEGED_IMAGE_TAG'] = '0.30.0-alpha.0';
-        process.env['MIRROR_IMAGE_TAG'] = '0.64.0-beta2';
+        process.env['MIRROR_IMAGE_TAG'] = '0.64.0-rc1';
         logger.trace(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
 
         // start local-node
