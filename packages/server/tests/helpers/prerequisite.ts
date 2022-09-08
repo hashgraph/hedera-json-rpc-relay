dcd0ae24a743595bd9d92b43f961faa2fca42433
Nana Essilfie-Conduah <nana@swirldslabs.com>
2022-09-08 11:19:33 -0500

    Bump mirror node 0.64.0
    Signed-off-by: Nana Essilfie-Conduah <nana@swirldslabs.com>

--- packages/server/tests/helpers/prerequisite.ts
+++ packages/server/tests/helpers/prerequisite.ts
@@ -13,7 +13,7 @@ const RELAY_URL = process.env.E2E_RELAY_HOST || LOCAL_RELAY_URL;
   if (USE_LOCAL_NODE) {
     process.env['NETWORK_NODE_IMAGE_TAG'] = '0.30.0-alpha.0';
     process.env['HAVEGED_IMAGE_TAG'] = '0.30.0-alpha.0';
-    process.env['MIRROR_IMAGE_TAG'] = '0.64.0-beta2';
+    process.env['MIRROR_IMAGE_TAG'] = '0.64.0';
     console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
 
     // start relay, stop relay instance in local-node
