98aa9d8cb63e090bb81defcf18c745ec17ba6de0
georgi-l95 <glazarov95@gmail.com>
2022-09-09 10:18:50 +0300

    change
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- packages/server/tests/acceptance/index.spec.ts
+++ packages/server/tests/acceptance/index.spec.ts
@@ -117,22 +117,25 @@ describe('RPC Server Acceptance Tests', function () {
         // set env variables for docker images until local-node is updated
         process.env['NETWORK_NODE_IMAGE_TAG'] = '0.30.0-alpha.1';
         process.env['HAVEGED_IMAGE_TAG'] = '0.30.0-alpha.1';
-        process.env['MIRROR_IMAGE_TAG'] = '0.64.0-beta2';
-
-        logger.trace(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
-
+        process.env['MIRROR_IMAGE_TAG'] = '0.64.0';
+      
+        console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
+      
         // start local-node
-
-        logger.debug('Installing local node...');
+      
+        console.log('Installing local node...');
         shell.exec(`npm install @hashgraph/hedera-local && hedera stop`);
-
-        logger.debug('Turning on compression...');
+      
+        console.log('Turning on compression...');
         shell.exec(`echo 'hedera.recordStream.compressFilesOnCreation=true' >> node_modules/@hashgraph/hedera-local/compose-network/network-node/data/config/bootstrap.properties`);
+        //on ubuntu
         shell.exec(`sed -i 's/      STREAM_EXTENSION: "rcd"/      STREAM_EXTENSION: "rcd.gz"/' node_modules/@hashgraph/hedera-local/docker-compose.yml`);
-
-        logger.debug('Start local node');
+        //on mac
+        // shell.exec(`sed -i '' 's/      STREAM_EXTENSION: "rcd"/      STREAM_EXTENSION: "rcd.gz"/' node_modules/@hashgraph/hedera-local/docker-compose.yml`);
+        
+        console.log('Start local node');
         shell.exec(`hedera start -d`);
-        logger.trace('Hedera Hashgraph local node env started');
+        console.log('Hedera Hashgraph local node env started');
     }
 
     function runLocalRelay() {
