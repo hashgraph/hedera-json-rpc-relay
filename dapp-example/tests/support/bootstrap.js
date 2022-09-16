41e0714deae5ca6bf0f788c201bf615c3de4b38c
georgi-l95 <glazarov95@gmail.com>
2022-09-16 17:19:01 +0300

    Add IERC transferFrom to HTS Precompile Acceptance Tests (#523)
    Add IERC transferFrom and approveNFT coverage
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- dapp-example/tests/support/bootstrap.js
+++ dapp-example/tests/support/bootstrap.js
@@ -112,8 +112,8 @@ const transferHTSToken = async function(accountId, tokenId) {
 };
 
 (async () => {
-  //wait for 10 sec, to be sure that local node is loaded, before trying to create accounts
-  await new Promise(r => setTimeout(r, 10000));
+  //wait for 20 sec, to be sure that local node is loaded, before trying to create accounts
+  await new Promise(r => setTimeout(r, 20000));
   let mainPrivateKeyString = process.env.PRIVATE_KEY;
   if (mainPrivateKeyString === '') {
     mainPrivateKeyString = HederaSDK.PrivateKey.generateECDSA().toStringRaw()
