4fe5421e7076104aeeae9117346bbd6c88afad95
georgi-l95 <glazarov95@gmail.com>
2022-11-14 22:11:31 +0200

    lower network node version
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- packages/server/tests/helpers/prerequisite.ts
+++ packages/server/tests/helpers/prerequisite.ts
@@ -11,8 +11,8 @@ const RELAY_URL = process.env.E2E_RELAY_HOST || LOCAL_RELAY_URL;
 
 (function () {
   if (USE_LOCAL_NODE) {
-    process.env['NETWORK_NODE_IMAGE_TAG'] = '0.32.0-alpha.4';
-    process.env['HAVEGED_IMAGE_TAG'] = '0.32.0-alpha.4';
+    process.env['NETWORK_NODE_IMAGE_TAG'] = '0.32.0-alpha.1';
+    process.env['HAVEGED_IMAGE_TAG'] = '0.32.0-alpha.1';
     process.env['MIRROR_IMAGE_TAG'] = '0.67.3';
     
     console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
