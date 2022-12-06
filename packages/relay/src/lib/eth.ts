31378de08b5d8497debd69d2931f2fa79fdd4e7a
georgi-l95 <glazarov95@gmail.com>
2022-12-06 02:01:33 +0200

    add unit tests
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- packages/relay/src/lib/eth.ts
+++ packages/relay/src/lib/eth.ts
@@ -918,8 +918,8 @@ export class EthImpl implements Eth {
     this.logger.trace(`${requestIdPrefix} call(hash=${JSON.stringify(call)}, blockParam=${blockParam})`, call, blockParam);
     // The "to" address must always be 42 chars.
     if (!call.to || call.to.length != 42) {
-      throw new Error(requestIdPrefix+
-        " Invalid Contract Address: '" + call.to + "'. Expected length of 42 chars but was" + call.to ? call.to.length : call.to
+      const callToExist = call.to && call.to.length ? " Expected length of 42 chars but was " + call.to.length + "." : '';
+      throw new Error(requestIdPrefix+ "Invalid Contract Address: '" + call.to + "'." + callToExist
       );
     }
 
