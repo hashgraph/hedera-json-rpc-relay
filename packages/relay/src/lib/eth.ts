0aa3c5a3ca1d8239ae941cbe916979c6e3d4bac6
Nikolay Atanasow <n.atanasow94@gmail.com>
2022-12-19 13:00:58 +0200

    Pad slot value on comparison (#770)
    * Pad slot value on comparison
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Add additional check
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Add tests
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit test
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Fix test
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Trigger ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Trigger ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit test
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit test
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit test
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit test
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Bump versions
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Try to fix test
    
    Signed-off-by: Maksim Dimitrov <dimitrov.maksim@gmail.com>
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    Signed-off-by: Maksim Dimitrov <dimitrov.maksim@gmail.com>
    Co-authored-by: Maksim Dimitrov <dimitrov.maksim@gmail.com>

--- packages/relay/src/lib/eth.ts
+++ packages/relay/src/lib/eth.ts
@@ -504,9 +504,11 @@ export class EthImpl implements Eth {
           }
           if (EthImpl.isArrayNonEmpty(contractResultDetails.state_changes)) {
             // filter the state changes to match slot and return value
-            const stateChange = contractResultDetails.state_changes.find(stateChange => stateChange.slot === slot);
-            result = stateChange.value_written;
-          } 
+            const stateChange = contractResultDetails.state_changes.find(stateChange => stateChange.slot === EthImpl.toHex32Byte(slot));
+            if (stateChange) {
+              result = stateChange.value_written;
+            }
+          }
         })
         .catch((e: any) => {
           this.logger.error(
@@ -1125,6 +1127,10 @@ export class EthImpl implements Eth {
 
   private static blockTagIsLatestOrPending = (tag) => {
     return tag == null || tag === EthImpl.blockLatest || tag === EthImpl.blockPending;
+  }
+
+  private static toHex32Byte(input: string): string {
+    return input.length === 66 ? input : EthImpl.emptyHex + this.prune0x(input).padStart(64, '0');
   }
 
   /**
