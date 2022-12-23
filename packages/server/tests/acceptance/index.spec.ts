e3ec7ca5d2b98c5ab41196c1571e3a27f0b62746
georgi-l95 <glazarov95@gmail.com>
2022-12-23 23:23:53 +0200

    bump packages version
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- packages/server/tests/acceptance/index.spec.ts
+++ packages/server/tests/acceptance/index.spec.ts
@@ -125,9 +125,9 @@ describe('RPC Server Acceptance Tests', function () {
 
     function runLocalHederaNetwork() {
         // set env variables for docker images until local-node is updated
-        process.env['NETWORK_NODE_IMAGE_TAG'] = '0.32.0';
-        process.env['HAVEGED_IMAGE_TAG'] = '0.32.0';
-        process.env['MIRROR_IMAGE_TAG'] = '0.70.1';
+        process.env['NETWORK_NODE_IMAGE_TAG'] = '0.33.2';
+        process.env['HAVEGED_IMAGE_TAG'] = '0.33.2';
+        process.env['MIRROR_IMAGE_TAG'] = '0.70.2';
     
         console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
         
