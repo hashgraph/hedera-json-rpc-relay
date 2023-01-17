b3b765040159a8b1f164c6d0cbd2e971d0333688
Nikolay Atanasow <n.atanasow94@gmail.com>
2023-01-17 11:13:34 +0200

    Investigate dapp tests failures (#809)
    * Fix tests
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Bump node version
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Add debug tunnel
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit package.json
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Add debug tunnel
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Test ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Add debug tunnel
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit timeouts
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Edit node version
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    * Test ci
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>
    
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

---
+++ dapp-example/synpress.js
@@ -0,0 +1,40 @@
+const { defineConfig } = require('cypress');
+const path = require('path');
+const synpressPath = path.dirname(require.resolve('@synthetixio/synpress'));
+
+module.exports = defineConfig({
+  userAgent: 'synpress',
+  fixturesFolder: `${synpressPath}/fixtures`,
+  screenshotsFolder: 'screenshots',
+  videosFolder: 'videos',
+  chromeWebSecurity: true,
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  video: false,
+  screenshotOnRunFailure: false,
+  defaultCommandTimeout: 180000,
+  pageLoadTimeout: 40000,
+  requestTimeout: 40000,
+  responseTimeout: 50000,
+  taskTimeout: 60000,
+  env: {
+    coverage: false
+  },
+  retries: {
+    runMode: 3,
+    openMode: 0
+  },
+  e2e: {
+    setupNodeEvents: require(`${synpressPath}/plugins/index`),
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'tests/e2e/specs/**/*.{js,jsx,ts,tsx}',
+    supportFile: 'tests/e2e/support.js'
+  },
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'spec, mocha-junit-reporter',
+    'mochaJunitReporterOptions': {
+      'includePending': true
+    }
+  }
+});
