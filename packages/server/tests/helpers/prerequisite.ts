ca4b1626c1ce3a0a4e992da527bee64634bc4520
georgi-l95 <glazarov95@gmail.com>
2023-01-20 19:36:02 +0200

    Bump acceptance test node images (#824)
    bump images
    
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>
    
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- packages/server/tests/helpers/prerequisite.ts
+++ packages/server/tests/helpers/prerequisite.ts
@@ -11,9 +11,9 @@ const RELAY_URL = process.env.E2E_RELAY_HOST || LOCAL_RELAY_URL;
 
 (function () {
   if (USE_LOCAL_NODE) {
-    process.env['NETWORK_NODE_IMAGE_TAG'] = '0.33.2';
-    process.env['HAVEGED_IMAGE_TAG'] = '0.33.2';
-    process.env['MIRROR_IMAGE_TAG'] = '0.72.0';
+    process.env['NETWORK_NODE_IMAGE_TAG'] = '0.35.0-alpha.1';
+    process.env['HAVEGED_IMAGE_TAG'] = '0.35.0-alpha.1';
+    process.env['MIRROR_IMAGE_TAG'] = '0.73.0-rc1';
 
     console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
 
