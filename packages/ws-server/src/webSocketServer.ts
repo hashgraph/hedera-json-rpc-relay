dc736eca44b46400740a9881104d8bd48f0be527
ebadiere <ebadiere@gmail.com>
2023-03-09 09:51:22 -0700

    927 - Added socket connection limits.
    Signed-off-by: ebadiere <ebadiere@gmail.com>

--- packages/ws-server/src/webSocketServer.ts
+++ packages/ws-server/src/webSocketServer.ts
@@ -46,10 +46,19 @@ const LOGGER_PREFIX = 'WebSocket:';
 
 const CHAIN_ID = relay.eth().chainId();
 const DEFAULT_ERROR = predefined.INTERNAL_ERROR();
+const MAX_CONNECTIONS = parseInt(process.env.CONNECTION_LIMIT || '10');
+let connectedClients = 0;
 
 app.ws.use((ctx) => {
     ctx.websocket.id = relay.subs()?.generateId();
     logger.info(`${LOGGER_PREFIX} new connection ${ctx.websocket.id}`);
+
+    if (connectedClients >= MAX_CONNECTIONS) {
+        ctx.websocket.send("Connections exceeded");
+        ctx.websocket.close();
+        return;
+    }
+    connectedClients++;
 
     ctx.websocket.on('message', async (msg) => {
         ctx.websocket.id = relay.subs()?.generateId();
@@ -98,6 +107,7 @@ app.ws.use((ctx) => {
     ctx.websocket.on('error', console.error);
 
     ctx.websocket.on('close', function () {
+        connectedClients--;
         relay.subs()?.unsubscribe(ctx.websocket);
         console.log('stopping client interval');
     });
