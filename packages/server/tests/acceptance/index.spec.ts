cf58abd7ae2194994a204ca4ad0a82b578eb9f60
georgi-l95 <glazarov95@gmail.com>
2023-03-27 14:56:29 +0300

    bump mirror-node version
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- packages/server/tests/acceptance/index.spec.ts
+++ packages/server/tests/acceptance/index.spec.ts
@@ -134,7 +134,7 @@ describe('RPC Server Acceptance Tests', function () {
         // set env variables for docker images until local-node is updated
         process.env['NETWORK_NODE_IMAGE_TAG'] = '0.36.0-alpha.2';
         process.env['HAVEGED_IMAGE_TAG'] = '0.36.0-alpha.2';
-        process.env['MIRROR_IMAGE_TAG'] = '0.76.1';
+        process.env['MIRROR_IMAGE_TAG'] = '0.77.0-rc2';
 
         console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
 
