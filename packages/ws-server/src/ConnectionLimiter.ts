8df7dfb2cbe42103ec41657788c83a3ae84edb34
Ivo Yankov <ivo@devlabs.bg>
2023-05-10 09:43:19 +0300

    Add metrics to the Websocket server (#1135)
    * feat: add metrics to ConnectionLimiter
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * feat: add metrics and health http endpoints
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * fix: acceptance test CI
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * feat: add more metrics
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * feat: add more metrics
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * nit: new line
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * docs: added metrics port to docs
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * chore: fixed test setup
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    * chore: add removeSingleMetric for all metrics
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    
    ---------
    
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- packages/ws-server/src/ConnectionLimiter.ts
+++ packages/ws-server/src/ConnectionLimiter.ts
@@ -20,6 +20,7 @@
 
 import { Logger } from "pino";
 import {WebSocketError} from "@hashgraph/json-rpc-relay";
+import {Histogram, Gauge, Registry, Counter} from "prom-client";
 
 type IpCounter = {
     [key: string]: number;
@@ -31,13 +32,62 @@ export default class ConnectionLimiter {
     private connectedClients: number;
     private clientIps: IpCounter;
     private logger: Logger;
+    private activeConnectionsGauge: Gauge;
+    private ipConnectionsGauge: Gauge;
+    private ipConnectionLimitCounter: Counter;
+    private connectionLimitCounter: Counter;
+    private connectionTTLCounter: Counter;
+    private register: Registry;
 
-    constructor(logger: Logger) {
+    constructor(logger: Logger, register: Registry) {
         this.logger = logger;
+        this.register = register;
         this.connectedClients = 0;
         this.clientIps = {};
-    }
 
+        const activeConnectionsMetric = 'rpc_websocket_active_connections';
+        this.register.removeSingleMetric(activeConnectionsMetric);
+
+        this.activeConnectionsGauge = new Gauge({
+            name: activeConnectionsMetric,
+            help: 'Relay websocket active connections',
+            registers: [register]
+        });
+
+        const ipConnectionsMetric = 'rpc_websocket_active_connections_per_ip';
+        this.register.removeSingleMetric(ipConnectionsMetric);
+        this.ipConnectionsGauge = new Gauge({
+            name: ipConnectionsMetric,
+            help: 'Relay websocket active connections by ip',
+            labelNames: ['ip'],
+            registers: [register]
+        });
+
+        const connectionLimitMetric = 'rpc_websocket_total_connection_limit_enforced';
+        this.register.removeSingleMetric(connectionLimitMetric);
+        this.connectionLimitCounter = new Counter({
+            name: connectionLimitMetric,
+            help: 'Relay websocket total connection limits enforced',
+            registers: [register]
+        });
+
+        const ipConnectionLimitMetric = 'rpc_websocket_total_connection_limit_by_ip_enforced';
+        this.register.removeSingleMetric(ipConnectionLimitMetric);
+        this.ipConnectionLimitCounter = new Counter({
+            name: ipConnectionLimitMetric,
+            help: 'Relay websocket total connection limits by ip enforced',
+            labelNames: ['ip'],
+            registers: [register]
+        });
+
+        const connectionTTLLimitMetric = 'rpc_websocket_total_connection_limit_by_ttl_enforced';
+        this.register.removeSingleMetric(connectionTTLLimitMetric);
+        this.connectionTTLCounter = new Counter({
+            name: connectionTTLLimitMetric,
+            help: 'Relay websocket total connection ttl limits enforced',
+            registers: [register]
+        });
+    }
 
     public incrementCounters(ctx) {
         const {ip} = ctx.request;
@@ -53,21 +103,27 @@ export default class ConnectionLimiter {
         ctx.websocket.ipCounted = true;
 
         ctx.websocket.subscriptions = 0;
+
+        this.activeConnectionsGauge.set(this.connectedClients);
+        this.ipConnectionsGauge.labels(ip).set(this.clientIps[ip]);
     }
 
     public decrementCounters(ctx) {
         if (ctx.websocket.ipCounted) {
             const {ip} = ctx.request;
             this.clientIps[ip]--;
+            this.ipConnectionsGauge.labels(ip).set(this.clientIps[ip]);
             if (this.clientIps[ip] === 0) delete this.clientIps[ip];
         }
         this.connectedClients--;
+        this.activeConnectionsGauge.set(this.connectedClients);
     }
 
     public applyLimits(ctx) {
         // Limit total connections
         if (this.connectedClients > parseInt(process.env.WS_CONNECTION_LIMIT || '10')) {
             this.logger.info(`Closing connection ${ctx.websocket.id} due to exceeded maximum connections (${process.env.WS_CONNECTION_LIMIT})`);
+            this.connectionLimitCounter.inc();
             ctx.websocket.close(CONNECTION_LIMIT_EXCEEDED.code, CONNECTION_LIMIT_EXCEEDED.message);
             return;
         }
@@ -78,6 +134,7 @@ export default class ConnectionLimiter {
         const limitPerIp = parseInt(process.env.WS_CONNECTION_LIMIT_PER_IP || '10');
         if (this.clientIps[ip] && this.clientIps[ip] > limitPerIp) {
             this.logger.info(`Closing connection ${ctx.websocket.id} due to exceeded maximum connections from a single IP (${this.clientIps[ip]}) for address ${ip}`);
+            this.ipConnectionLimitCounter.labels(ip).inc();
             ctx.websocket.close(CONNECTION_IP_LIMIT_EXCEEDED.code, CONNECTION_IP_LIMIT_EXCEEDED.message);
             return;
         }
@@ -88,6 +145,7 @@ export default class ConnectionLimiter {
             if (ctx.websocket.readyState !== 3) { // 3 = CLOSED, Avoid closing already closed connections
                 this.logger.debug(`Closing connection ${ctx.websocket.id} due to reaching TTL of ${maxConnectionTTL}ms`);
                 try {
+                    this.connectionTTLCounter.inc();
                     ctx.websocket.close(TTL_EXPIRED.code, TTL_EXPIRED.message);
                 } catch (e) {
                     this.logger.error(`${ctx.websocket.id}: ${e}`);
