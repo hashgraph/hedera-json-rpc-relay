0270ca72266e513e8666fb65915e3a25c65873db
Ivo Yankov <ivo@devlabs.bg>
2023-05-11 11:53:06 +0300

    chore: resolve conflicts
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- packages/ws-server/src/index.ts
+++ packages/ws-server/src/index.ts
@@ -21,8 +21,8 @@
 import { app, httpApp } from './webSocketServer';
 
 async function main() {
-  await app.listen({ port: process.env.WEB_SOCKET_PORT || 8546 });
-  await httpApp.listen({ port: process.env.WEB_SOCKET_HTTP_PORT || 8547 });
+  app.listen({ port: process.env.WEB_SOCKET_PORT || 8546 });
+  httpApp.listen({ port: process.env.WEB_SOCKET_HTTP_PORT || 8547 });
 }
 
 (async () => {
