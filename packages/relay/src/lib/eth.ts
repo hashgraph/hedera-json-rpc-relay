1e5633b04cb506684f9cfce62bca8be214525f56
Alfredo Gutierrez <alfredo@swirldslabs.com>
2023-06-21 18:32:52 -0600

    Fixes a "Cannot read properties of null" bug when increasing metrics counter.
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>

--- packages/relay/src/lib/eth.ts
+++ packages/relay/src/lib/eth.ts
@@ -155,10 +155,10 @@ export class EthImpl implements Eth {
   private readonly chain: string;
 
   /**
-   * The counter used to track the number of active contract execution requests.
+   * The ethExecutionsCounter used to track the number of active contract execution requests.
    * @private
    */
-  private counter: Counter;
+  private ethExecutionsCounter: Counter;
 
   /**
    * Create a new Eth implementation.
@@ -183,10 +183,10 @@ export class EthImpl implements Eth {
     this.cache = cache;
     if (!cache) this.cache = new LRU(this.options);
 
-    this.counter = this.initCounter(registry);
+    this.ethExecutionsCounter = this.initEthExecutionCounter(registry);
   }
 
-  private initCounter(register: Registry) {
+  private initEthExecutionCounter(register: Registry) {
     const metricCounterName = 'rpc_relay_eth_executions';
     register.removeSingleMetric(metricCounterName);
     return new Counter({
@@ -467,9 +467,9 @@ export class EthImpl implements Eth {
     const requestIdPrefix = formatRequestIdMessage(requestId);
     this.logger.trace(`${requestIdPrefix} estimateGas(transaction=${JSON.stringify(transaction)}, _blockParam=${_blockParam})`);
 
-    if ("data" in transaction){
-      this.counter.labels(EthImpl.ethEstimateGas, transaction.data.substring(0,10)).inc();
-    }
+    if (transaction?.data?.length >= 10)
+      this.ethExecutionsCounter.labels(EthImpl.ethEstimateGas, transaction.data.substring(0,10)).inc();
+
 
     let gas = EthImpl.gasTxBaseCost;
     try {
@@ -1113,7 +1113,8 @@ export class EthImpl implements Eth {
    */
   async sendRawTransaction(transaction: string, requestId?: string): Promise<string | JsonRpcError> {
     const requestIdPrefix = formatRequestIdMessage(requestId);
-    this.counter.labels(EthImpl.ethSendRawTransaction, transaction.substring(0,10)).inc();
+    if(transaction?.length >= 10)
+      this.ethExecutionsCounter.labels(EthImpl.ethSendRawTransaction, transaction.substring(0,10)).inc();
 
     let interactingEntity = '';
     let originatingAddress = '';
@@ -1181,9 +1182,9 @@ export class EthImpl implements Eth {
     const requestIdPrefix = formatRequestIdMessage(requestId);
     this.logger.trace(`${requestIdPrefix} call(hash=${JSON.stringify(call)}, blockParam=${blockParam})`, call, blockParam);
 
-    if ("data" in call){
-      this.counter.labels(EthImpl.ethCall, call.data.substring(0,10)).inc();
-    }
+    if (call.data?.length >= 10)
+      this.ethExecutionsCounter.labels(EthImpl.ethCall, call.data.substring(0,10)).inc();
+
     
     const to = await this.performCallChecks(call, blockParam, requestId);
 
