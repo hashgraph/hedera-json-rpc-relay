328f3d758deb9d98c325282194dc6eea6350cc74
Nana Essilfie-Conduah <nana@swirldslabs.com>
2023-07-10 14:36:56 -0500

    Default to empty string on metric key
    Signed-off-by: Nana Essilfie-Conduah <nana@swirldslabs.com>

--- packages/relay/src/lib/clients/clientCache.ts
+++ packages/relay/src/lib/clients/clientCache.ts
@@ -86,7 +86,7 @@ export class ClientCache {
     public get(key: string, callingMethod: string, requestIdPrefix?: string): any {
         const value = this.cache.get(key);
         if (value !== undefined) {
-            this.cacheKeyGauge.labels(key, ClientCache.getCacheLabel, callingMethod || '').inc(1);
+            this.cacheKeyGauge.labels('', ClientCache.getCacheLabel, callingMethod || '').inc(1);
             this.logger.trace(`${requestIdPrefix} returning cached value ${key}:${JSON.stringify(value)} on ${callingMethod} call`);
             return value;
         }
@@ -98,7 +98,7 @@ export class ClientCache {
         const resolvedTtl = ttl ?? this.options.ttl;    
         this.logger.trace(`${requestIdPrefix} caching ${key}:${JSON.stringify(value)} for ${resolvedTtl} ms`);
         this.cache.set(key, value, { ttl: resolvedTtl });
-        this.cacheKeyGauge.labels(key, ClientCache.setCacheLabel, callingMethod || '').inc(1);
+        this.cacheKeyGauge.labels('', ClientCache.setCacheLabel, callingMethod || '').inc(1);
     }
 
     public purgeStale(): void {
