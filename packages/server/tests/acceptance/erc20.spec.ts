736dbc497e785174c8e79e024cc5988223136cdf
ebadiere <ebadiere@gmail.com>
2023-07-11 23:06:43 -0600

    Updated test expectation based on behavior in CI.
    Signed-off-by: ebadiere <ebadiere@gmail.com>

--- packages/server/tests/acceptance/erc20.spec.ts
+++ packages/server/tests/acceptance/erc20.spec.ts
@@ -266,7 +266,7 @@ describe('@erc20 Acceptance Tests', async function () {
                                         // eth_estimateGas gets called by ethers
                                         // so we need to catch the error and check that the reason is the expected one,
                                         // in addition to validating the CALL_EXCEPTION   
-                                        expect(extractRevertReason(e.error.reason)).to.be.equal('ERC20: insufficient allowance');
+                                        expect(extractRevertReason(e.error.reason)).to.be.equal('ERC20: transfer amount exceeds balance');
                                      }
                                     });
                                 });
