3f9cb5bc79fea7cc209957ca45aa8c4cdfb064bd
Ivo Yankov <ivo@devlabs.bg>
2023-07-21 11:20:13 +0300

    chore: bump versions
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- packages/server/tests/helpers/prerequisite.ts
+++ packages/server/tests/helpers/prerequisite.ts
@@ -11,9 +11,9 @@ const RELAY_URL = process.env.E2E_RELAY_HOST || LOCAL_RELAY_URL;
 
 (function () {
   if (USE_LOCAL_NODE) {
-    process.env['NETWORK_NODE_IMAGE_TAG'] = '0.39.0-alpha.0';
-    process.env['HAVEGED_IMAGE_TAG'] = '0.39.0-alpha.0';
-    process.env['MIRROR_IMAGE_TAG'] = '0.82.1';
+    process.env['NETWORK_NODE_IMAGE_TAG'] = '0.40.1';
+    process.env['HAVEGED_IMAGE_TAG'] = '0.40.1';
+    process.env['MIRROR_IMAGE_TAG'] = '0.84.0';
 
     console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
 
