1f790a56813497d1d2ed20d37cd458aece740159
nikolay <n.atanasow94@gmail.com>
2023-07-31 11:27:48 +0300

    Bump image
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- packages/server/tests/acceptance/index.spec.ts
+++ packages/server/tests/acceptance/index.spec.ts
@@ -139,7 +139,7 @@ describe('RPC Server Acceptance Tests', function () {
         // set env variables for docker images until local-node is updated
         process.env['NETWORK_NODE_IMAGE_TAG'] = '0.39.0-alpha.0';
         process.env['HAVEGED_IMAGE_TAG'] = '0.39.0-alpha.0';
-        process.env['MIRROR_IMAGE_TAG'] = '0.85.0-rc1';
+        process.env['MIRROR_IMAGE_TAG'] = '0.85.0-rc4';
 
         console.log(`Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`);
 
