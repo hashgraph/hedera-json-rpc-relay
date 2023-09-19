3ffbc58736318ad290c5bbb9edded3cf941f7349
Alfredo Gutierrez <alfredo@swirldslabs.com>
2023-09-19 11:45:34 -0600

    Dependencies upgrades, hashgraph/hedera-local, hashgraph/sdk, images for hedera mirror-node and network-node when running acceptance tests
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>

--- packages/server/tests/acceptance/index.spec.ts
+++ packages/server/tests/acceptance/index.spec.ts
@@ -140,9 +140,9 @@ describe('RPC Server Acceptance Tests', function () {
 
   function runLocalHederaNetwork() {
     // set env variables for docker images until local-node is updated
-    process.env['NETWORK_NODE_IMAGE_TAG'] = '0.41.0-alpha.3';
-    process.env['HAVEGED_IMAGE_TAG'] = '0.41.0-alpha.3';
-    process.env['MIRROR_IMAGE_TAG'] = '0.88.0';
+    process.env['NETWORK_NODE_IMAGE_TAG'] = '0.42.0-alpha.4';
+    process.env['HAVEGED_IMAGE_TAG'] = '0.42.0-alpha.4';
+    process.env['MIRROR_IMAGE_TAG'] = '0.88.0-beta2';
 
     console.log(
       `Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`,
