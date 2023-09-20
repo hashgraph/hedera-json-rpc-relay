e34c86b80a6121b074a5f31fb5c9460fc5897a4a
georgi-l95 <glazarov95@gmail.com>
2023-09-20 15:51:45 +0300

    update local-node version
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- packages/server/tests/helpers/prerequisite.ts
+++ packages/server/tests/helpers/prerequisite.ts
@@ -13,7 +13,7 @@ const RELAY_URL = process.env.E2E_RELAY_HOST || LOCAL_RELAY_URL;
   if (USE_LOCAL_NODE) {
     process.env['NETWORK_NODE_IMAGE_TAG'] = '0.41.0-alpha.3';
     process.env['HAVEGED_IMAGE_TAG'] = '0.41.0-alpha.3';
-    process.env['MIRROR_IMAGE_TAG'] = '0.86.0-beta1';
+    process.env['MIRROR_IMAGE_TAG'] = '0.88.0';
 
     console.log(
       `Docker container versions, services: ${process.env['NETWORK_NODE_IMAGE_TAG']}, mirror: ${process.env['MIRROR_IMAGE_TAG']}`,
