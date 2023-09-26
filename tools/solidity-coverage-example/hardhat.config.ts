900a2bd589281ccc0a86a56c68c4f2784f9243bc
Stefan Stefanov <stefan.stefanooov@gmail.com>
2023-09-26 16:45:54 +0300

    Adding Example project with solidity-coverage lib
    Signed-off-by: Stefan Stefanov <stefan.stefanooov@gmail.com>

---
+++ tools/solidity-coverage-example/hardhat.config.ts
@@ -0,0 +1,10 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'solidity-coverage';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.19',
+};
+
+export default config;
