9b9edfecf2565c4e35514600316841f1f5ff82f5
anastasiya-kovaliova <144098930+anastasiya-kovaliova@users.noreply.github.com>
2023-10-02 19:01:44 +0300

    Add basic positive tests for contract creation with transactions of t… (#1758)
    * Add basic positive tests for contract creation with transactions of type 1 and 2
    
    Signed-off-by: Anastasia Kovaliova <anastasiya.kovaliova@limechain.tech>
    
    * add the nonce as it was set to be the same on each transaction
    
    Signed-off-by: Anastasia Kovaliova <anastasiya.kovaliova@limechain.tech>
    
    ---------
    
    Signed-off-by: Anastasia Kovaliova <anastasiya.kovaliova@limechain.tech>
    Co-authored-by: Eric Badiere <ebadiere@gmail.com>

--- packages/server/tests/acceptance/rpc_batch1.spec.ts
+++ packages/server/tests/acceptance/rpc_batch1.spec.ts
@@ -33,6 +33,7 @@ import { predefined } from '../../../relay/src/lib/errors/JsonRpcError';
 import Constants from '../../../relay/src/lib/constants';
 import RelayCalls from '../../tests/helpers/constants';
 const Address = RelayCalls;
+import basicContract from '../../tests/contracts/Basic.json';
 import { numberTo0x, prepend0x } from '../../../../packages/relay/src/formatters';
 
 describe('@api-batch-1 RPC Server Acceptance Tests', function () {
@@ -934,6 +935,50 @@ describe('@api-batch-1 RPC Server Acceptance Tests', function () {
         expect(info.contract_id).to.not.be.null;
         expect(info).to.have.property('created_contract_ids');
         expect(info.created_contract_ids.length).to.be.equal(1);
+      });
+
+      it('should execute "eth_sendRawTransaction" of type 1 and deploy a real contract', async function () {
+        //omitting the "to" and "nonce" fields when creating a new contract
+        const transaction = {
+          ...defaultLegacy2930TransactionData,
+          value: 0,
+          data: basicContract.bytecode,
+          nonce: await relay.getAccountNonce(accounts[2].address, requestId),
+        };
+
+        const signedTx = await accounts[2].wallet.signTransaction(transaction);
+        const transactionHash = await relay.sendRawTransaction(signedTx, requestId);
+        const info = await mirrorNode.get(`/contracts/results/${transactionHash}`, requestId);
+        expect(info).to.have.property('contract_id');
+        expect(info.contract_id).to.not.be.null;
+        expect(info).to.have.property('created_contract_ids');
+        expect(info.created_contract_ids.length).to.be.equal(1);
+        expect(info.max_fee_per_gas).to.eq('0x');
+        expect(info.max_priority_fee_per_gas).to.eq('0x');
+        expect(info).to.have.property('access_list');
+      });
+
+      it('should execute "eth_sendRawTransaction" of type 2 and deploy a real contract', async function () {
+        //omitting the "to" and "nonce" fields when creating a new contract
+        const transaction = {
+          ...defaultLondonTransactionData,
+          value: 0,
+          data: basicContract.bytecode,
+          nonce: await relay.getAccountNonce(accounts[2].address, requestId),
+        };
+
+        const signedTx = await accounts[2].wallet.signTransaction(transaction);
+        const transactionHash = await relay.sendRawTransaction(signedTx, requestId);
+        const info = await mirrorNode.get(`/contracts/results/${transactionHash}`, requestId);
+        expect(info).to.have.property('contract_id');
+        expect(info.contract_id).to.not.be.null;
+        expect(info).to.have.property('max_fee_per_gas');
+        expect(info).to.have.property('max_priority_fee_per_gas');
+        expect(info).to.have.property('created_contract_ids');
+        expect(info.created_contract_ids.length).to.be.equal(1);
+        expect(info).to.have.property('type');
+        expect(info.type).to.be.equal(2);
+        expect(info).to.have.property('access_list');
       });
 
       it('should execute "eth_sendRawTransaction" and deploy a contract with more than 2 HBAR transaction fee and less than max transaction fee', async function () {
