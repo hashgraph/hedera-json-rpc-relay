6128c76b40580963ee6c7a3a8e98b061fd67c4e7
Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
2023-11-09 17:05:31 +0200

    e2e(tests): exclude a failing test due to a bug
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>

--- packages/server/tests/acceptance/estimateGasPrecompile.spec.ts
+++ packages/server/tests/acceptance/estimateGasPrecompile.spec.ts
@@ -21,44 +21,28 @@
 import { ethers } from 'ethers';
 import { expect } from 'chai';
 import { Utils } from '../helpers/utils';
+import { AliasAccount } from '../clients/servicesClient';
 import EstimatePrecompileContractJson from '../contracts/EstimatePrecompileContract.json';
 import ERC20MockJson from '../contracts/ERC20Mock.json';
-import { AliasAccount } from '../clients/servicesClient';
-import constants from '../../../../packages/relay/src/lib/constants';
 import Constants from '../../tests/helpers/constants';
 import RelayCalls from '../../../../packages/server/tests/helpers/constants';
-import { assert } from 'console';
 import ERC721MockJson from '../contracts/ERC721Mock.json';
 
-describe.only('EstimatePrecompileContract tests', function () {
+describe('EstimatePrecompileContract tests', function () {
   const signers: AliasAccount[] = [];
   const prefix = '0x';
   const SERVER_ERROR = 'SERVER_ERROR';
   const CALL_EXCEPTION = 'CALL_EXCEPTION';
-  const UNSUPPORTED_OPERATION = 'UNSUPPORTED_OPERATION';
   let contract: ethers.Contract;
-  let randomAddress: string;
   let contractReceipt;
   let EstimatePrecompileContractAddress;
-  let ERC20Mock;
   let requestId;
   let tokenAddress;
   let nftAddress;
   let NftSerialNumber;
-  let tokenContract;
-  let mainContractAddress;
-  let HTSTokenContractAddress;
-  let NftHTSTokenContractAddress;
-  let HTSTokenContract;
-  let mainContract;
-  let mainContractOwner;
-  let mainContractReceiverWalletFirst;
   let estimateContractSigner0;
-  let estimateContractSigner1;
-  let estimateContractSigner2;
   let estimateContract;
 
-  const TX_SUCCESS_CODE = BigInt(22);
   const accounts: AliasAccount[] = [];
   const { servicesNode, mirrorNode, relay }: any = global;
 
@@ -114,11 +98,8 @@ describe.only('EstimatePrecompileContract tests', function () {
     contractReceipt = await servicesNode.deployContract(EstimatePrecompileContractJson, 5_000_000);
     EstimatePrecompileContractAddress = contractReceipt.contractId.toSolidityAddress();
 
-    //  const contractDeploy = await Utils.deployContractWithEthers([], EstimatePrecompileContractJson, signers[0].wallet, relay);
-    //EstimatePrecompileContractAddress = contractDeploy.target.replace('0x', '');
     contract = new ethers.Contract(
-      // Utils.add0xPrefix(contractDeploy.target),
-      '0x' + EstimatePrecompileContractAddress,
+      prefix + EstimatePrecompileContractAddress,
       EstimatePrecompileContractJson.abi,
       signers[0].wallet,
     );
@@ -146,32 +127,17 @@ describe.only('EstimatePrecompileContract tests', function () {
     );
 
     contract = new ethers.Contract(
-      '0x' + EstimatePrecompileContractAddress,
-      // Utils.add0xPrefix(contractDeploy.target),
+      prefix + EstimatePrecompileContractAddress,
       EstimatePrecompileContractJson.abi,
       accounts[0].wallet,
     );
 
-    // Create token and nft contracts
+    // Create token and NFT contracts
     tokenAddress = await createFungibleToken();
     nftAddress = await createNft();
-    tokenContract = new ethers.Contract(tokenAddress, EstimatePrecompileContractJson.abi, accounts[0].wallet);
 
-    // Associate token and nft to accounts
-    /*estimateContractSigner0 = new ethers.Contract("0x" + EstimatePrecompileContractAddress, EstimatePrecompileContractJson.abi, accounts[0].wallet);
-        console.log("This is estimateContractSigner0" + estimateContractSigner0)
-        const tx1 = await estimateContractSigner0.associateTokenExternal(
-            "0x" + EstimatePrecompileContractAddress,
-            tokenAddress,
-            Constants.GAS.LIMIT_1_000_000,
-        );
-        expect(
-            (await tx1.wait()).logs.filter((e) => e.fragment.name === Constants.HTS_CONTRACT_EVENTS.ResponseCode)[0].args[0]
-        ).to.equal(TX_SUCCESS_CODE);
-        console.log("This is tx1 " + tx1);
-*/
-    estimateContractSigner1 = new ethers.Contract(
-      '0x' + EstimatePrecompileContractAddress,
+    let estimateContractSigner1 = new ethers.Contract(
+      prefix + EstimatePrecompileContractAddress,
       EstimatePrecompileContractJson.abi,
       accounts[1].wallet,
     );
@@ -181,8 +147,8 @@ describe.only('EstimatePrecompileContract tests', function () {
       Constants.GAS.LIMIT_1_000_000,
     );
 
-    estimateContractSigner2 = new ethers.Contract(
-      '0x' + EstimatePrecompileContractAddress,
+    let estimateContractSigner2 = new ethers.Contract(
+      prefix + EstimatePrecompileContractAddress,
       EstimatePrecompileContractJson.abi,
       accounts[2].wallet,
     );
@@ -192,22 +158,12 @@ describe.only('EstimatePrecompileContract tests', function () {
       Constants.GAS.LIMIT_1_000_000,
     );
 
-    estimateContractSigner1 = new ethers.Contract(
-      '0x' + EstimatePrecompileContractAddress,
-      EstimatePrecompileContractJson.abi,
-      accounts[1].wallet,
-    );
     const t1_nft = await estimateContractSigner1.associateTokenExternal(
       accounts[1].wallet.address,
       nftAddress,
       Constants.GAS.LIMIT_1_000_000,
     );
 
-    estimateContractSigner2 = new ethers.Contract(
-      '0x' + EstimatePrecompileContractAddress,
-      EstimatePrecompileContractJson.abi,
-      accounts[2].wallet,
-    );
     const tx2_nft = await estimateContractSigner2.associateTokenExternal(
       accounts[2].wallet.address,
       nftAddress,
@@ -219,58 +175,24 @@ describe.only('EstimatePrecompileContract tests', function () {
     await contract.grantTokenKycExternal(nftAddress, accounts[0].wallet.address, { gasLimit: 500_000 });
     await contract.grantTokenKycExternal(nftAddress, accounts[1].wallet.address, { gasLimit: 500_000 });
     await contract.grantTokenKycExternal(nftAddress, accounts[2].wallet.address, { gasLimit: 500_000 });
-    await contract.grantTokenKycExternal(nftAddress, '0x' + EstimatePrecompileContractAddress, { gasLimit: 500_000 });
+    await contract.grantTokenKycExternal(nftAddress, prefix + EstimatePrecompileContractAddress, { gasLimit: 500_000 });
 
-    //fungible
+    //Fungible
     await contract.grantTokenKycExternal(tokenAddress, accounts[0].wallet.address, { gasLimit: 500_000 });
     await contract.grantTokenKycExternal(tokenAddress, accounts[1].wallet.address, { gasLimit: 500_000 });
     await contract.grantTokenKycExternal(tokenAddress, accounts[2].wallet.address, { gasLimit: 500_000 });
-    await contract.grantTokenKycExternal(tokenAddress, '0x' + EstimatePrecompileContractAddress, { gasLimit: 500_000 });
-    /*
-        console.log("This is tx2 " + tx2);
-        expect(
-            (await tx2.wait()).logs.filter((e) => e.fragment.name === Constants.HTS_CONTRACT_EVENTS.ResponseCode)[0].args[0]
-        ).to.equal(TX_SUCCESS_CODE);
-
-        estimateContractSigner0 = new ethers.Contract("0x" + EstimatePrecompileContractAddress, EstimatePrecompileContractJson.abi, accounts[0].wallet);
-        const tx3 = await estimateContractSigner0.associateTokenExternal(
-            "0x" + EstimatePrecompileContractAddress,
-            nftAddress,
-            Constants.GAS.LIMIT_1_000_000,
-        );
-        console.log("This is tx3 " + tx3);
-        expect(
-            (await tx3.wait()).logs.filter((e) => e.fragment.name === Constants.HTS_CONTRACT_EVENTS.ResponseCode)[0].args[0]
-        ).to.equal(TX_SUCCESS_CODE);
-
-        estimateContractSigner1 = new ethers.Contract("0x" + EstimatePrecompileContractAddress, EstimatePrecompileContractJson.abi, accounts[1].wallet);
-        const tx4 = await estimateContractSigner1.associateTokenExternal(
-            accounts[1].wallet.address,
-            nftAddress,
-            Constants.GAS.LIMIT_1_000_000,
-        );
-        console.log("This is tx4 " + tx4);
-        expect(
-            (await tx4.wait()).logs.filter((e) => e.fragment.name === Constants.HTS_CONTRACT_EVENTS.ResponseCode)[0].args[0]
-        ).to.equal(TX_SUCCESS_CODE); */
+    await contract.grantTokenKycExternal(tokenAddress, prefix + EstimatePrecompileContractAddress, {
+      gasLimit: 500_000,
+    });
   });
+
   const baseGasCheck = (estimatedGasValue, expectedValue: number) => {
     // handle deviation of 20%
     expect(Number(estimatedGasValue)).to.be.lessThan(expectedValue * 1.4);
   };
 
-  it('test', async function () {
-    expect(true).to.be.true;
-  });
-
   //EGP-001
   it('should call estimateGas with associate function for fungible token', async function () {
-    console.log('this is estimate pr contract: ' + EstimatePrecompileContractAddress);
-    console.log('this is signer0 address: ' + accounts[0].wallet.address);
-    console.log('this is signer1 address: ' + accounts[1].wallet.address);
-    console.log('this is token address: ' + tokenAddress);
-    console.log('this is nft address: ' + nftAddress);
-    console.log('this is the estimate pr contract: ' + EstimatePrecompileContractAddress);
     const tx = await contract.associateTokenExternal.populateTransaction(signers[0].address, tokenAddress);
     const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 786166);
@@ -423,9 +345,7 @@ describe.only('EstimatePrecompileContract tests', function () {
   });
 
   //EGP-016
-  xit('should call estimateGas with setApprovalForAll function', async function () {
-    // e31b839c
-
+  it('should call estimateGas with setApprovalForAll function', async function () {
     const tx = await contract.setApprovalForAllExternal.populateTransaction(
       nftAddress,
       accounts[1].wallet.address,
@@ -435,10 +355,8 @@ describe.only('EstimatePrecompileContract tests', function () {
     baseGasCheck(estimateGasResponse, 786166);
   });
 
-  //EGP-017 //temp disabled due to spam in the log
-  xit('should call estimateGas with transferFromNFT function', async function () {
-    // 9bc4d35
-
+  //EGP-017
+  it('should call estimateGas with transferFromNFT function', async function () {
     const nftSerial = await mintNFT();
     const NFTokenContract = new ethers.Contract(nftAddress, ERC721MockJson.abi, accounts[0].wallet);
 
@@ -468,57 +386,40 @@ describe.only('EstimatePrecompileContract tests', function () {
     negativeScenarioVerification(tx, CALL_EXCEPTION);
   });
 
-  //EGP-019 //Excluded due to a bug
-  it.only('should call estimateGas with transferFrom function', async function () {
+  //EGP-019 //The test is excluded due to a bug in the Mirror-node https://github.com/hashgraph/hedera-mirror-node/issues/7192
+  xit('should call estimateGas with transferFrom function', async function () {
     let amount = 1;
     let account0Wallet = await mirrorNode.get(`/accounts/${accounts[0].wallet.address}`, requestId);
     let account1Wallet = await mirrorNode.get(`/accounts/${accounts[1].wallet.address}`, requestId);
 
     let account0LongZero = Utils.idToEvmAddress(account0Wallet.account);
-    console.log('This is account0LongZero:' + account0LongZero);
     let account1LongZero = Utils.idToEvmAddress(account1Wallet.account);
-    console.log('This is account1LongZero:' + account1LongZero);
 
     const ERC20Contract = new ethers.Contract(tokenAddress, ERC20MockJson.abi, accounts[0].wallet);
-    console.log(await ERC20Contract.balanceOf(contract.target));
-    console.log(await ERC20Contract.balanceOf(accounts[0].wallet.address));
-    console.log(await ERC20Contract.balanceOf(accounts[1].wallet.address));
-    //which has ballance - from this
-    //to contract.target
+
     const erc20Accounts0 = ERC20Contract.connect(accounts[0].wallet);
     // @ts-ignore
     const tx1 = await erc20Accounts0.approve(contract.target, 2);
     await tx1.wait();
 
     const allowance = await ERC20Contract.allowance(accounts[0].wallet.address, contract.target);
-    console.log(allowance);
     await new Promise((r) => setTimeout(r, 5000));
-    //then this approveExternal
-    // await contract.approveExternal(tokenAddress, accounts[1].wallet.address, amount);
-    console.log(await ERC20Contract.balanceOf(contract.target));
-    console.log(await ERC20Contract.balanceOf(accounts[0].wallet.address));
-    console.log(await ERC20Contract.balanceOf(accounts[1].wallet.address));
+    await contract.approveExternal(tokenAddress, accounts[1].wallet.address, amount);
 
     await new Promise((r) => setTimeout(r, 5000));
-    console.log(await ERC20Contract.balanceOf(contract.target));
-    console.log(await ERC20Contract.balanceOf(accounts[0].wallet.address));
-    console.log(await ERC20Contract.balanceOf(accounts[1].wallet.address));
-
     const tx2 = await contract.transferFromExternal.populateTransaction(
       tokenAddress,
       account0LongZero,
       account1LongZero,
       amount,
     );
-    console.log('This is tx2 : ' + tx2);
+
     const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx2]);
-    console.log(estimateGasResponse);
     baseGasCheck(estimateGasResponse, 786166);
   });
 
   //EGP-020
   it('should call estimateGas with transferFrom function with more than the approved allowance - negative', async function () {
-    //approved 1 in the previous test so amount defined is higher
     let amount = 10;
 
     const tx = await contract.transferFromExternal.populateTransaction(
@@ -545,19 +446,14 @@ describe.only('EstimatePrecompileContract tests', function () {
 
   //EGP-022
   it('should call estimateGas with transferToken function', async function () {
-    console.log('this is estimate pr contract: ' + EstimatePrecompileContractAddress);
-    console.log('this is signer0 address: ' + accounts[0].wallet.address);
-    console.log('this is signer1 address: ' + accounts[1].wallet.address);
-    console.log('this is token address: ' + tokenAddress);
-    console.log('this is nft address: ' + nftAddress);
     let amount = 1;
+
     const tx = await contract.transferTokenExternal.populateTransaction(
       tokenAddress,
       accounts[0].wallet.address,
       accounts[1].wallet.address,
       amount,
     );
-    console.log('data ' + tx);
     const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 43265);
   });
