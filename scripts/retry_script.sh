11d7a17444f62dfb925c439fd5b55c26bc46c3a3
Iliya Savov <isavov@users.noreply.github.com>
2023-11-30 18:09:45 +0200

    chore: comments
    Signed-off-by: Iliya Savov <isavov@users.noreply.github.com>

--- scripts/retry_script.sh
+++ scripts/retry_script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -v
 set +e
+# Temporary workaround until issue in local node is resolved
 hedera stop
 sleep 5
 ls -lah $(dirname $(readlink -f $(which hedera)))/../network-logs
 sudo rm -rf $(dirname $(readlink -f $(which hedera)))/../network-logs
 sleep 5
+
+
 hedera restart -d
 docker stop json-rpc-relay json-rpc-relay-ws
