a20c23461d3b58db1aaaea3bf0585e90bf4a0565
Iliya Savov <isavov@users.noreply.github.com>
2023-11-30 18:19:33 +0200

    docs: add reference to local-node issue
    Signed-off-by: Iliya Savov <isavov@users.noreply.github.com>

--- scripts/retry_script.sh
+++ scripts/retry_script.sh
@@ -2,6 +2,7 @@
 set -v
 set +e
 # Temporary workaround until issue in local node is resolved
+# https://github.com/hashgraph/hedera-local-node/issues/308#issue-1647448101
 hedera stop
 sleep 5
 ls -lah $(dirname $(readlink -f $(which hedera)))/../network-logs
