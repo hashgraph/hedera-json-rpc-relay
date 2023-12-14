adc0918b54342012c20718bbfa43fe21fe00a261
Alfredo Gutierrez <alfredo@swirldslabs.com>
2023-12-14 17:17:57 -0600

    Created a separate new metric for tracking the method results, simila… (#1887)
    Created a separate new metric for tracking the method results, similar to the existing metric for the responses, but this one is only for batch requests, and pending refactor of metrics that would allow to reuse metrics across several classes, a metricsService class.
    
    Signed-off-by: Alfredo Gutierrez <alfredo@swirldslabs.com>
    Signed-off-by: ebadiere <ebadiere@gmail.com>

---
+++ packages/server/src/koaJsonRpc/lib/HttpStatusCodeAndMessage.ts
@@ -0,0 +1,27 @@
+export class HttpStatusCodeAndMessage {
+  public statusCode: number;
+  public StatusName: string;
+
+  constructor(statusCode: number, statusName: string) {
+    this.statusCode = statusCode;
+    this.StatusName = statusName;
+  }
+}
+
+const INTERNAL_ERROR = 'INTERNAL ERROR';
+const INVALID_PARAMS_ERROR = 'INVALID PARAMS ERROR';
+const INVALID_REQUEST = 'INVALID REQUEST';
+const IP_RATE_LIMIT_EXCEEDED = 'IP RATE LIMIT EXCEEDED';
+const JSON_RPC_ERROR = 'JSON RPC ERROR';
+const CONTRACT_REVERT = 'CONTRACT REVERT';
+const METHOD_NOT_FOUND = 'METHOD NOT FOUND';
+
+export const RpcErrorCodeToStatusMap = {
+  '-32603': new HttpStatusCodeAndMessage(500, INTERNAL_ERROR),
+  '-32008': new HttpStatusCodeAndMessage(200, CONTRACT_REVERT),
+  '-32600': new HttpStatusCodeAndMessage(400, INVALID_REQUEST),
+  '-32602': new HttpStatusCodeAndMessage(400, INVALID_PARAMS_ERROR),
+  '-32601': new HttpStatusCodeAndMessage(400, METHOD_NOT_FOUND),
+  '-32605': new HttpStatusCodeAndMessage(409, IP_RATE_LIMIT_EXCEEDED),
+  default: new HttpStatusCodeAndMessage(400, JSON_RPC_ERROR),
+};
