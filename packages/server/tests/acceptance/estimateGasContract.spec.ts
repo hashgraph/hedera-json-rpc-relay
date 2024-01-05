29f665b690fe6bf6d39c7984d0b6896db2a7b6dc
nickeynikolovv <96113577+nickeynikolovv@users.noreply.github.com>
2024-01-05 20:54:39 +0200

    test: [1600] Add tests for estimate gas with precompiles (#2002)
    * e2e(tests):add tests for estimateGas with Precompiles
    
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    
    * e2e(tests):add tests for estimateGas with Precompiles
    
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    
    * e2e(tests): exclude a failing test due to a bug
    
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    
    * e2e(tests):resolve comments
    
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    
    * 1600 - Adapted the existing to use deviation and actual gas and added tests for estimateGas with precompiles (#1930)
    
    * Cleared the latest changes for the initial fork PR
    
    Signed-off-by: Nikolay Nikolov <nikolay.nikolov@limechain.tech>
    
    * Added tests for
    - Grant, Revoke token KYC
    - Freeze/Unfreeze and nested function
    - Delete token
    
    Signed-off-by: Nikolay Nikolov <nikolay.nikolov@limechain.tech>
    
    * Added test for ERC transferFrom with more than the allowance
    
    Signed-off-by: Nikolay Nikolov <nikolay.nikolov@limechain.tech>
    
    ---------
    
    Signed-off-by: Nikolay Nikolov <nikolay.nikolov@limechain.tech>
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    
    * Fixed comments
    - Removed .only from the describe
    - Removed unneeded comment
    
    Signed-off-by: Nikolay Nikolov <nikolay.nikolov@limechain.tech>
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    
    * [1600] Add tests for estimate gas with precompiles - Comments fixes
    - removed unneeded .dbg files
    
    Signed-off-by: Nikolay Nikolov <nikolay.nikolov@limechain.tech>
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    
    ---------
    
    Signed-off-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    Signed-off-by: Nikolay Nikolov <nikolay.nikolov@limechain.tech>
    Co-authored-by: Aleksandrina Todorova <aleksandrina.aleksieva@limechain.tech>

--- packages/server/tests/acceptance/estimateGasContract.spec.ts
+++ packages/server/tests/acceptance/estimateGasContract.spec.ts
@@ -24,6 +24,7 @@ import { Utils } from '../helpers/utils';
 import EstimateGasContractJson from '../contracts/EstimateGasContract.json';
 import { AliasAccount } from '../clients/servicesClient';
 import constants from '../../../../packages/relay/src/lib/constants';
+import RelayCalls from '../../../../packages/server/tests/helpers/constants';
 
 describe('EstimateGasContract tests', function () {
   const signers: AliasAccount[] = [];
@@ -105,7 +106,9 @@ describe('EstimateGasContract tests', function () {
   for (const test of basicTests) {
     it(test.name, async function () {
       baseGasCheck(
-        await relay.call('eth_estimateGas', [await contract[test.functionName].populateTransaction()]),
+        await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [
+          await contract[test.functionName].populateTransaction(),
+        ]),
         test.expectedGas,
       );
     });
@@ -113,26 +116,30 @@ describe('EstimateGasContract tests', function () {
 
   it('#006 Function with .balance (uint256)', async function () {
     const tx = await contract.addressBalance.populateTransaction(signers[0].address);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x57ac);
   });
+
   it('#007 Function that accepts an argument and change contract slot information by updating global contract field with the passed argument', async function () {
     const tx = await contract.updateCounter.populateTransaction(5644);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x6a4e);
   });
+
   it('#008 Function that successfully deploys a new smart contract via CREATE op code', async function () {
     const tx = await contract.deployViaCreate.populateTransaction();
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0xd48b);
   });
+
   it('#009 Function that successfully deploys a new smart contract via CREATE2 op code', async function () {
     const tx = await contract.deployViaCreate2.populateTransaction();
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0xdcc4);
   });
+
   it('#015 "data" from request body with wrong method signature', async function () {
-    const estimateGasResponse = await relay.call('eth_estimateGas', [
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [
       {
         data: '0xffffffff',
         to: contract.target,
@@ -141,8 +148,9 @@ describe('EstimateGasContract tests', function () {
     ]);
     baseGasCheck(estimateGasResponse, 0x61a80);
   });
+
   it('#016 "data" from request body with wrong encoded parameter', async function () {
-    const estimateGasResponse = await relay.call('eth_estimateGas', [
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [
       {
         data: '0x3ec4de35',
         to: contract.target,
@@ -151,8 +159,9 @@ describe('EstimateGasContract tests', function () {
     ]);
     baseGasCheck(estimateGasResponse, 0x61a80);
   });
+
   it('#017 non existing "from" from request body', async function () {
-    const estimateGasResponse = await relay.call('eth_estimateGas', [
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [
       {
         data: '0x0ec1551d',
         to: Utils.add0xPrefix(contract.target),
@@ -161,8 +170,9 @@ describe('EstimateGasContract tests', function () {
     ]);
     baseGasCheck(estimateGasResponse, 0x567d);
   });
+
   it('#018 non existing "to" from request body', async function () {
-    const estimateGasResponse = await relay.call('eth_estimateGas', [
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [
       {
         data: '0x0ec1551d',
         to: randomAddress,
@@ -171,92 +181,105 @@ describe('EstimateGasContract tests', function () {
     ]);
     baseGasCheck(estimateGasResponse, 0x61a80);
   });
+
   it('#019 Function that makes a call to a method to invalid smart contract', async function () {
     const tx = await contract.callToInvalidContract.populateTransaction(randomAddress);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x62de);
   });
+
   it('#020 Function that makes a delegate call to a method to invalid smart contract', async function () {
     const tx = await contract.delegateCallToInvalidContract.populateTransaction(randomAddress);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x6299);
   });
+
   it('#021 Function that makes a static call to a method to invalid smart contract', async function () {
     const tx = await contract.staticCallToInvalidContract.populateTransaction(randomAddress);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x62dc);
   });
+
   it('#022 Function that makes a call code to a method to invalid smart contract', async function () {
     const tx = await contract.callCodeToInvalidContract.populateTransaction(randomAddress);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x6187);
   });
+
   it('#023 Execute .call to external contract', async function () {
     const tx = await contract.callExternalFunctionNTimes.populateTransaction(1, contract.target);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x6c46);
     const txN = await contract.callExternalFunctionNTimes.populateTransaction(100, contract.target);
-    const estimateGasResponseN = await relay.call('eth_estimateGas', [txN]);
+    const estimateGasResponseN = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [txN]);
     baseGasCheck(estimateGasResponseN, 0x232d2);
     expect(Number(estimateGasResponseN)).to.be.greaterThan(Number(estimateGasResponse));
   });
+
   it('#024 Execute .delegatecall to external contract', async function () {
     const tx = await contract.delegatecallExternalFunctionNTimes.populateTransaction(1, contract.target);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x6c17);
     const txN = await contract.delegatecallExternalFunctionNTimes.populateTransaction(100, contract.target);
-    const estimateGasResponseN = await relay.call('eth_estimateGas', [txN]);
+    const estimateGasResponseN = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [txN]);
     baseGasCheck(estimateGasResponseN, 0x2317a);
     expect(Number(estimateGasResponseN)).to.be.greaterThan(Number(estimateGasResponse));
   });
+
   it('#025 Execute state update method', async function () {
     const tx = await contract.updateStateNTimes.populateTransaction(1);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x681e);
     const txN = await contract.updateStateNTimes.populateTransaction(100);
-    const estimateGasResponseN = await relay.call('eth_estimateGas', [txN]);
+    const estimateGasResponseN = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [txN]);
     baseGasCheck(estimateGasResponse, 0xd23b);
     expect(Number(estimateGasResponseN)).to.be.greaterThan(Number(estimateGasResponse));
   });
+
   it('#026 Execute view .call to external contract', async function () {
     const tx = await contract.callExternalViewFunctionNTimes.populateTransaction(1, contract.target);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x5a92);
     const txN = await contract.callExternalViewFunctionNTimes.populateTransaction(100, contract.target);
-    const estimateGasResponseN = await relay.call('eth_estimateGas', [txN]);
+    const estimateGasResponseN = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [txN]);
     baseGasCheck(estimateGasResponseN, 0x194d0);
     expect(Number(estimateGasResponseN)).to.be.greaterThan(Number(estimateGasResponse));
   });
+
   it('#028 Execute reentrancy with transfer', async function () {
     const tx = await contract.reentrancyWithTransfer.populateTransaction(
       randomAddress,
       constants.TINYBAR_TO_WEIBAR_COEF,
     );
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x61a80);
   });
+
   it('#029 Execute reentrancy with call', async function () {
     const tx = await contract.reentrancyWithCall.populateTransaction(randomAddress, constants.TINYBAR_TO_WEIBAR_COEF);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0xe4ba);
   });
+
   it('#030 Execute and validate gasleft()', async function () {
     const tx = await contract.getGasLeft.populateTransaction();
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     expect(Number(estimateGasResponse)).to.be.lessThan(constants.TX_BASE_COST * 1.2);
   });
+
   it('#031 Execute positive nested calls)', async function () {
     const tx = await contract.nestedCalls.populateTransaction(1, 10, contract.target);
-    const estimateGasResponse = await relay.call('eth_estimateGas', [tx]);
+    const estimateGasResponse = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx]);
     baseGasCheck(estimateGasResponse, 0x90bc);
   });
+
   it('#032 Execute limited nested calls)', async function () {
     const tx500 = await contract.nestedCalls.populateTransaction(1, 500, contract.target);
-    const estimateGasResponse500 = await relay.call('eth_estimateGas', [tx500]);
+    const estimateGasResponse500 = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx500]);
     const tx750 = await contract.nestedCalls.populateTransaction(1, 750, contract.target);
-    const estimateGasResponse750 = await relay.call('eth_estimateGas', [tx750]);
+    const estimateGasResponse750 = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx750]);
     const tx1000 = await contract.nestedCalls.populateTransaction(1, 1000, contract.target);
-    const estimateGasResponse1000 = await relay.call('eth_estimateGas', [tx1000]);
+    const estimateGasResponse1000 = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [tx1000]);
     expect(estimateGasResponse500).to.equals(estimateGasResponse750).to.equal(estimateGasResponse1000);
   });
 });
