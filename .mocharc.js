7a093e2564fc83ca343c9598e903c2bb3d3d7aff
Iliya Savov <isavov@users.noreply.github.com>
2024-01-17 14:48:06 +0200

    feat: generate junit reports for acceptance tests
    Signed-off-by: Iliya Savov <isavov@users.noreply.github.com>

--- .mocharc.js
+++ .mocharc.js
@@ -1,5 +1,14 @@
 'use strict';
 module.exports = {
-    'forbid-only': process.env.CI,
+    'forbid-only': Boolean(process.env.CI),
     color: true,
+    'fail-zero': Boolean(process.env.CI),
+    reporter: "mocha-multi-reporters",
+    'reporter-options': "configFile=.mocharc.js",
+    'reporterEnabled': "spec, mocha-junit-reporter",
+    "mochaJunitReporterReporterOptions": {
+        "mochaFile": "test-results.[hash].xml",
+        "includePending": true,
+        "outputs": true
+    },
 };
