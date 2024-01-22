54feedb146852c2caafb919ad4d066c34eee3391
Iliya Savov <117984128+isavov@users.noreply.github.com>
2024-01-22 08:10:30 +0200

    chore: add test reports to acceptance tests workflow (#2049)
    Signed-off-by: Iliya Savov <isavov@users.noreply.github.com>

--- .mocharc.js
+++ .mocharc.js
@@ -1,5 +1,14 @@
 'use strict';
 module.exports = {
-    'forbid-only': process.env.CI,
+    'forbid-only': Boolean(process.env.CI),
     color: true,
+    'fail-zero': Boolean(process.env.CI),
+    reporter: "mocha-multi-reporters",
+    'reporter-options': `configFile=${__filename}`,
+    'reporterEnabled': "spec, mocha-junit-reporter",
+    "mochaJunitReporterReporterOptions": {
+        "mochaFile": `${__dirname}/test-results.[hash].xml`,
+        "includePending": true,
+        "outputs": true
+    },
 };
