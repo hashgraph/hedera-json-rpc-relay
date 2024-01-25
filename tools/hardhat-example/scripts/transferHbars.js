29678fc6bad0f4ff6a51c945fb37a37614edbf85
Luis Mastrangelo <luis@swirldslabs.com>
2024-01-25 14:48:36 -0300

    Fix Hardhat custom tasks
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- tools/hardhat-example/scripts/transferHbars.js
+++ tools/hardhat-example/scripts/transferHbars.js
@@ -24,7 +24,7 @@ module.exports = async (amount = 100_000_000_000) => {
   const wallet = (await ethers.getSigners())[0];
   const walletReceiver = (await ethers.getSigners())[1];
 
-  console.log(`Balance before tx: ${await walletReceiver.getBalance()}`);
+  console.log(`Balance before tx: ${await walletReceiver.provider.getBalance(walletReceiver.address)}`);
   // Keep in mind that TINYBAR to WEIBAR coefficient is 10_000_000_000
   await wallet.sendTransaction({
     to: walletReceiver.address,
@@ -32,5 +32,5 @@ module.exports = async (amount = 100_000_000_000) => {
   });
   //delay of 3 sec before fetching the new balance, because often it's too fast when querying and the balance is the same like before transfer transaciton
   await new Promise((r) => setTimeout(r, 3000));
-  console.log(`Balance after tx: ${await walletReceiver.getBalance()}`);
+  console.log(`Balance after tx: ${await walletReceiver.provider.getBalance(walletReceiver.address)}`);
 };
