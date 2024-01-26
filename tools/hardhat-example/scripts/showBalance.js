c288615a33e08dfa172df7754aa764bad12eeb71
Luis Mastrangelo <acuarica@users.noreply.github.com>
2024-01-26 17:41:17 -0300

    docs: add verification instructions in Hardhat example (#2056)
    * Add verification instructions in Hardhat example
    
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>
    
    Moreover it includes a new env key to set up the user private key on testnet,
    and updates Hardhat dependencies.
    
    * Set up a well-formed private key in `.env.example`
    
    This is to avoid failing in CI given CI uses this example to run.
    
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>
    
    * Fix Hardhat custom tasks
    
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>
    
    * Fix Hardhat tests
    
    There are breaking changes when migrating ethers.js v5 to v6
    (this is because we had to update `hardhat-toolbox` for the custom verification
    to work)
    - `getBalance` needs to be invoked from the `provider`
    - Use `target` instead of `address` to get the address of a contract
    - Use `METHOD.staticCall` instead of `callStatic.METHOD`
    - Use `getCreateAddress` instead of `getContractAddress`
    - Use `logs` instead of `events` to get the emitted events in a
      receipt
    - Use native BigInt instead of BigNumber
    
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>
    
    ---------
    
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- tools/hardhat-example/scripts/showBalance.js
+++ tools/hardhat-example/scripts/showBalance.js
@@ -22,7 +22,7 @@ const { ethers } = require('hardhat');
 
 module.exports = async () => {
   const wallet = (await ethers.getSigners())[0];
-  const balance = (await wallet.getBalance()).toString();
+  const balance = (await wallet.provider.getBalance(wallet.address)).toString();
   console.log(`The address ${wallet.address} has ${balance} tinybars`);
 
   return balance;
