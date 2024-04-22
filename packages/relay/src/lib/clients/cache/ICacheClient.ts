68532b84ff60cae7556afe39f123db165b2b51b1
konstantinabl <konstantina.blajukova@gmail.com>
2024-04-22 13:59:49 +0300

    Update packages/relay/src/lib/clients/cache/ICacheClient.ts
    Co-authored-by: Victor Yanev <161485803+victor-yanev@users.noreply.github.com>
    Signed-off-by: konstantinabl <konstantina.blajukova@gmail.com>

--- packages/relay/src/lib/clients/cache/ICacheClient.ts
+++ packages/relay/src/lib/clients/cache/ICacheClient.ts
@@ -20,7 +20,7 @@
 
 export interface ICacheClient {
   get(key: string, callingMethod: string, requestIdPrefix?: string): Promise<any> | any;
-  set(key: string, value: any, callingMethod: string, ttl?: number, requestIdPrefix?: string): void;
+  set(key: string, value: any, callingMethod: string, ttl?: number, requestIdPrefix?: string): Promise<void> | void;
   delete(key: string, callingMethod: string, requestIdPrefix?: string): void;
   clear(): Promise<void> | void;
   multiSet(keyValuePairs: Record<string, any>, callingMethod: string, requestIdPrefix?: string): void;
