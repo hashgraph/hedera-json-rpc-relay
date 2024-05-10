bccd640468cb7d4cd2b14a12d84e00eefd502a16
Logan Nguyen <logan.nguyen@swirldslabs.com>
2024-05-09 20:35:20 -0400

    refactor: removed specific handlers for jsonrpc methods but added a shared method
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- packages/ws-server/src/controllers/index.ts
+++ packages/ws-server/src/controllers/index.ts
@@ -18,38 +18,7 @@
  *
  */
 
-import { handleEthCall } from './eth_call';
-import { handleEthGetLogs } from './eth_getLogs';
-import { handleEthGetCode } from './eth_getCode';
-import { handleEthGasPrice } from './eth_gasPrice';
 import { handleEthSubsribe } from './eth_subscribe';
 import { handleEthUnsubscribe } from './eth_unscribe';
-import { handleEthGetBalance } from './eth_getBalance';
-import { handleEthEstimateGas } from './eth_estimateGas';
-import { handleEthBlockNumber } from './eth_blockNumber';
-import { handleEthGetStorageAt } from './eth_getStorageAt';
-import { handleEthGetBlockByHash } from './eth_getBlockByHash';
-import { handleEthGetBlockByNumber } from './eth_getBlockByNumber';
-import { handleEthSendRawTransaction } from './eth_sendRawTransaction';
-import { handleEthGetTransactionCount } from './eth_getTransactionCount';
-import { handleEthGetTransactionByHash } from './eth_getTransactionByHash';
-import { handleEthGetTransactionReceipt } from './eth_getTransactionReceipt';
 
-export {
-  handleEthCall,
-  handleEthGetLogs,
-  handleEthGetCode,
-  handleEthSubsribe,
-  handleEthGasPrice,
-  handleEthGetBalance,
-  handleEthUnsubscribe,
-  handleEthBlockNumber,
-  handleEthEstimateGas,
-  handleEthGetStorageAt,
-  handleEthGetBlockByHash,
-  handleEthGetBlockByNumber,
-  handleEthSendRawTransaction,
-  handleEthGetTransactionCount,
-  handleEthGetTransactionByHash,
-  handleEthGetTransactionReceipt,
-};
+export { handleEthSubsribe, handleEthUnsubscribe };
