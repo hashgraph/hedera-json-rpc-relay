b63b22b737747e40d6be810e81f8c17c0f48a8fe
Eric Badiere <ebadiere@gmail.com>
2024-05-16 09:45:38 -0600

    fix: Applied fix using trimPrecedingZeros. (#2499)
    fix: fix for gasPrice.
    
    
    
    fix: Clean up
    
    Signed-off-by: ebadiere <ebadiere@gmail.com>

--- packages/relay/tests/lib/formatters.spec.ts
+++ packages/relay/tests/lib/formatters.spec.ts
@@ -36,6 +36,7 @@ import {
   weibarHexToTinyBarInt,
   isValidEthereumAddress,
   trimPrecedingZeros,
+  isHex,
 } from '../../src/formatters';
 import constants from '../../src/lib/constants';
 import { BigNumber as BN } from 'bignumber.js';
@@ -202,10 +203,10 @@ describe('Formatters', () => {
       expect(formattedResult.chainId).to.equal('0x12a');
       expect(formattedResult.from).to.equal('0x05fba803be258049a27b820088bab1cad2058871');
       expect(formattedResult.gas).to.equal('0x61a80');
-      expect(formattedResult.gasPrice).to.equal(null);
+      expect(formattedResult.gasPrice).to.equal('0x0');
       expect(formattedResult.hash).to.equal('0xfc4ab7133197016293d2e14e8cf9c5227b07357e6385184f1cd1cb40d783cfbd');
       expect(formattedResult.input).to.equal('0x08090033');
-      expect(formattedResult.maxPriorityFeePerGas).to.equal(null);
+      expect(formattedResult.maxPriorityFeePerGas).to.equal('0x0');
       expect(formattedResult.maxFeePerGas).to.equal('0x59');
       expect(formattedResult.nonce).to.equal('0x2');
       expect(formattedResult.r).to.equal('0x2af9d41244c702764ed86c5b9f1a734b075b91c4d9c65e78bc584b0e35181e42');
@@ -238,14 +239,14 @@ describe('Formatters', () => {
       expect(formattedResult.chainId).to.equal('0x12a');
       expect(formattedResult.from).to.equal('0x05fba803be258049a27b820088bab1cad2058871');
       expect(formattedResult.gas).to.equal('0x0');
-      expect(formattedResult.gasPrice).to.equal(null);
+      expect(formattedResult.gasPrice).to.equal('0x0');
       expect(formattedResult.hash).to.equal('0xfc4ab7133197016293d2e14e8cf9c5227b07357e6385184f1cd1cb40d783cfbd');
       expect(formattedResult.input).to.equal('0x08090033');
-      expect(formattedResult.maxPriorityFeePerGas).to.equal(null);
-      expect(formattedResult.maxFeePerGas).to.equal(null);
+      expect(formattedResult.maxPriorityFeePerGas).to.equal('0x0');
+      expect(formattedResult.maxFeePerGas).to.equal('0x0');
       expect(formattedResult.nonce).to.equal('0x0');
-      expect(formattedResult.r).to.equal(null);
-      expect(formattedResult.s).to.equal(null);
+      expect(formattedResult.r).to.equal('0x0');
+      expect(formattedResult.s).to.equal('0x0');
       expect(formattedResult.to).to.equal('0x0000000000000000000000000000000000000409');
       expect(formattedResult.transactionIndex).to.equal(null);
       expect(formattedResult.v).to.equal(`0x0`);
@@ -425,6 +426,43 @@ describe('Formatters', () => {
     it('should return false for an address with an undefined value', () => {
       const address = undefined;
       expect(isValidEthereumAddress(address)).to.equal(false);
+    });
+  });
+  describe('isHex Function', () => {
+    it('should return true for valid lowercase hexadecimal string', () => {
+      expect(isHex('0x1a3f')).to.be.true;
+    });
+
+    it('should return true for valid uppercase hexadecimal string', () => {
+      expect(isHex('0xABC')).to.be.true;
+    });
+
+    it('should return true for mixed-case hexadecimal string', () => {
+      expect(isHex('0xAbC123')).to.be.true;
+    });
+
+    it('should return false for string without 0x prefix', () => {
+      expect(isHex('1a3f')).to.be.false;
+    });
+
+    it('should return false for string with invalid characters', () => {
+      expect(isHex('0x1g3f')).to.be.false;
+    });
+
+    it('should return false for string with only 0x prefix', () => {
+      expect(isHex('0x')).to.be.false;
+    });
+
+    it('should return false for empty string', () => {
+      expect(isHex('')).to.be.false;
+    });
+
+    it('should return false for string with spaces', () => {
+      expect(isHex('0x 1a3f')).to.be.false;
+    });
+
+    it('should return true for a known gasPrice', () => {
+      expect(isHex('0x58')).to.be.true;
     });
   });
 });
