71975b9ede643ccc015b60afb163435bc5a4461f
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2024-06-10 16:48:00 +0200

    feat: Golang JSON RPC methods usage example.
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

---
+++ tools/golang-json-rpc-tests/README.md
@@ -0,0 +1,41 @@
+# Hedera JSON RPC - Golang Tests Project
+
+This project offers boilerplate code for using golang client methods on Hedera JSON RPC api.
+It can be configured to communicate with both community-hosted and local ([Hedera Local Node](https://github.com/hashgraph/hedera-local-node)) instances of the [Hedera JSON RPC Relay](https://github.com/hashgraph/hedera-json-rpc-relay).
+
+## Requirements
+Install go: https://go.dev/doc/install
+
+## Setup
+
+1. Clone this repo to your local machine:
+
+```shell
+git clone https://github.com/hashgraph/hedera-json-rpc-relay.git
+```
+
+2. Once you've cloned the repository, open your IDE terminal and navigate to the root directory of the project:
+
+```shell
+cd hedera-json-rpc-relay/tools/golang-json-rpc-tests
+```
+
+3. Run the following command to install all the necessary dependencies:
+
+```shell
+go get github.com/ethereum/go-ethereum/ethclient \
+       github.com/ethereum/go-ethereum \
+       github.com/ethereum/go-ethereum/accounts/abi/bind \
+       github.com/ethereum/go-ethereum/crypto \
+       github.com/joho/godotenv
+```
+
+4. Get your Hedera testnet account hex encoded private key from the [Hedera Developer Portal](https://portal.hedera.com/register) and update the `.env.example` `OPERATOR_PRIVATE_KEY`
+
+5. Copy `.env.example` to `.env`
+
+6. Run the test script from the root directory of the project. The default network is set to "testnet."
+
+```shell
+go run .
+```
---
+++ tools/golang-json-rpc-tests/main.go
@@ -0,0 +1,363 @@
+package main
+
+import (
+    "context"
+    "crypto/ecdsa"
+    "encoding/hex"
+    "flag"
+    "fmt"
+    "log"
+    "math/big"
+    "os"
+    "strings"
+
+    "github.com/ethereum/go-ethereum"
+    "github.com/ethereum/go-ethereum/accounts/abi/bind"
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/core/types"
+    "github.com/ethereum/go-ethereum/crypto"
+    "github.com/ethereum/go-ethereum/ethclient"
+    "github.com/joho/godotenv"
+)
+
+const (
+    mainnetEndpoint     = "https://mainnet.hashio.io/api"
+    testnetEndpoint     = "https://testnet.hashio.io/api"
+    previewnetEndpoint  = "https://previewnet.hashio.io/api"
+)
+
+func main() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file")
+    }
+    mainnet := flag.Bool("mainnet", false, "Use mainnet network")
+    previewnet := flag.Bool("previewnet", false, "Use previewnet network")
+    privateKeyHex := os.Getenv("OPERATOR_PRIVATE_KEY")
+    flag.Parse()
+    var endpointUrl string
+    switch {
+    case *mainnet:
+        endpointUrl = mainnetEndpoint
+    case *previewnet:
+        endpointUrl = previewnetEndpoint
+    default:
+        endpointUrl = testnetEndpoint
+    }
+    client, err := ethclient.Dial(endpointUrl)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println("Connected to Ethereum client")
+    chainId, err := client.ChainID(context.Background())
+    if err != nil {
+        log.Fatalf("Failed to get chain ID: %v", err)
+    }
+    fmt.Printf("Chain ID: %s\n", chainId.String())
+    privateKeyHex = strings.TrimPrefix(privateKeyHex, "0x")
+    privateKey, err := crypto.HexToECDSA(privateKeyHex)
+    if err != nil {
+        log.Fatalf("Failed to parse private key: %v", err)
+    }
+    publicKey := privateKey.Public().(*ecdsa.PublicKey)
+    fromAddress := crypto.PubkeyToAddress(*publicKey)
+    signedTx := testSendDummyTransaction(client, fromAddress, privateKey, chainId)
+    receipt := waitForTransaction(client, signedTx)
+    blockNumber := receipt.BlockNumber
+    blockHash := receipt.BlockHash
+    newestBlock := big.NewInt(0)
+    testFeeHistory(client, 5, newestBlock, []float64{10, 50, 90})
+    testBlockByNumber(client, blockNumber)
+    testTransactionByHash(client, signedTx.Hash().Hex())
+    testTransactionReceipt(client, signedTx.Hash().Hex())
+    testGetBalance(client, fromAddress)
+    testEthCall(client, fromAddress)
+    testEstimateGas(client, fromAddress)
+    testGetGasPrice(client)
+    testGetAccounts(client)
+    testBlockByHash(client, blockHash)
+    testFeeHistory(client, 5, newestBlock, []float64{10, 50, 90})
+    testGetBlockTransactionCountByHash(client, blockHash)
+    testGetBlockTransactionCountByNumber(client)
+    testCodeAt(client, fromAddress)
+    testGetLogs(client, fromAddress, nil)
+    testStorageAt(client, fromAddress, "0x0")
+    testGetTransactionByBlockHashAndIndex(client, blockHash, 0)
+    testGetTransactionByBlockNumberAndIndex(client, blockHash, 0)
+    testGetTransactionByHash(client, signedTx.Hash().Hex())
+    testGetTransactionCount(client)
+    testGetTransactionReceipt(client, signedTx.Hash().Hex())
+    testSyncing(client)
+}
+
+func testBlockByHash(client *ethclient.Client, blockHash common.Hash) {
+    block, err := client.BlockByHash(context.Background(), blockHash)
+    if err != nil {
+        log.Fatalf("Failed to get block by hash: %v", err)
+    }
+    fmt.Printf("Block by hash: %s\n", block.Hash().Hex())
+}
+
+func testBlockByNumber(client *ethclient.Client, blockNumber *big.Int) {
+    _, err := client.BlockByNumber(context.Background(), blockNumber)
+    if err != nil {
+        log.Fatalf("Failed to get block by number: %v", err)
+    }
+    fmt.Printf("Block by number\n")
+}
+
+func testTransactionByHash(client *ethclient.Client, txHash string) {
+    tx, isPending, err := client.TransactionByHash(context.Background(), common.HexToHash(txHash))
+    if err != nil {
+        log.Fatalf("Failed to get transaction by hash: %v", err)
+    }
+    fmt.Printf("Transaction by hash: %s (Pending: %t)\n", tx.Hash().Hex(), isPending)
+}
+
+func testTransactionReceipt(client *ethclient.Client, txHash string) {
+    receipt, err := client.TransactionReceipt(context.Background(), common.HexToHash(txHash))
+    if err != nil {
+        log.Fatalf("Failed to get transaction receipt: %v", err)
+    }
+    if receipt == nil {
+        fmt.Printf("Transaction receipt: null\n")
+    } else {
+        fmt.Printf("Transaction receipt: %d\n", receipt.Status)
+    }
+}
+
+func testGetBalance(client *ethclient.Client, fromAddress common.Address) {
+    balance, err := client.BalanceAt(context.Background(), fromAddress, nil)
+    if err != nil {
+        log.Fatalf("Failed to get balance: %v", err)
+    }
+    fmt.Printf("Account balance: %s\n", balance.String())
+}
+
+func testEthCall(client *ethclient.Client, fromAddress common.Address) {
+    msg := ethereum.CallMsg{
+        From: fromAddress,
+        To:   &fromAddress,
+        Data: []byte("0x"),
+    }
+    result, err := client.CallContract(context.Background(), msg, nil)
+    if err != nil {
+        log.Fatalf("Failed to call contract: %v", err)
+    }
+    fmt.Printf("eth_call result: %s\n", hex.EncodeToString(result))
+}
+
+func testEstimateGas(client *ethclient.Client, fromAddress common.Address) {
+    msg := ethereum.CallMsg{
+        From:  fromAddress,
+        To:    &fromAddress,
+        Value: big.NewInt(10000000000),
+        Gas:   21000,
+        Data:  nil,
+    }
+    gasEstimate, err := client.EstimateGas(context.Background(), msg)
+    if err != nil {
+        log.Fatalf("Failed to estimate gas: %v", err)
+    }
+    fmt.Printf("Gas estimate: %d\n", gasEstimate)
+}
+
+func testGetGasPrice(client *ethclient.Client) {
+    gasPrice, err := client.SuggestGasPrice(context.Background())
+    if err != nil {
+        log.Fatalf("Failed to get gas price: %v", err)
+    }
+    fmt.Printf("Gas price: %s\n", gasPrice.String())
+}
+
+func testSendDummyTransaction(client *ethclient.Client, fromAddress common.Address, privateKey *ecdsa.PrivateKey, chainId *big.Int) *types.Transaction {
+    nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+    if err != nil {
+        log.Fatalf("Failed to get transaction count: %v", err)
+    }
+    fmt.Printf("Transaction count (nonce): %d\n", nonce)
+
+    gasPrice, err := client.SuggestGasPrice(context.Background())
+    if err != nil {
+        log.Fatalf("Failed to get gas price: %v", err)
+    }
+
+    txData := &types.AccessListTx{
+        ChainID:    chainId,
+        Nonce:      nonce,
+        GasPrice:   gasPrice,
+        Gas:        21000,
+        To:         &fromAddress,
+        Value:      big.NewInt(10000000000), // 0.01 Ether
+        Data:       nil,
+        AccessList: types.AccessList{},
+    }
+
+    tx := types.NewTx(txData)
+    signer := types.NewEIP2930Signer(chainId)
+    signedTx, err := types.SignTx(tx, signer, privateKey)
+    if err != nil {
+        log.Fatalf("Failed to sign transaction: %v", err)
+    }
+
+    v, r, s := signedTx.RawSignatureValues()
+    fmt.Printf("R: %s\n", r.String())
+    fmt.Printf("S: %s\n", s.String())
+    fmt.Printf("V: %s\n", v.String())
+
+    err = client.SendTransaction(context.Background(), signedTx)
+    if err != nil {
+        log.Fatalf("Failed to send transaction: %v", err)
+    }
+    fmt.Printf("Sent raw transaction: %s\n", signedTx.Hash().Hex())
+
+    return signedTx
+}
+
+func testGetAccounts(client *ethclient.Client) {
+    rpcClient := client.Client()
+
+    var result []string
+    err := rpcClient.CallContext(context.Background(), &result, "eth_accounts")
+    if err != nil {
+        log.Fatalf("Failed to get accounts: %v", err)
+    }
+
+    for _, account := range result {
+        address := common.HexToAddress(account)
+        balance, err := client.BalanceAt(context.Background(), address, nil)
+        if (err != nil) {
+            fmt.Printf("Failed to get balance for account %s: %v\n", account, err)
+            continue
+        }
+        fmt.Printf("Account %s balance: %s\n", account, balance.String())
+    }
+}
+
+func testFeeHistory(client *ethclient.Client, blockCount uint64, newestBlock *big.Int, rewardPercentiles []float64) {
+    ctx := context.Background()
+
+    feeHistory, err := client.FeeHistory(ctx, blockCount, newestBlock, rewardPercentiles)
+    if err != nil {
+        log.Fatalf("Failed to get fee history: %v", err)
+    }
+
+    fmt.Printf("Fee History:\n")
+    fmt.Printf("Oldest Block: %s\n", feeHistory.OldestBlock.String())
+    fmt.Printf("Gas Used Ratio: %v\n", feeHistory.GasUsedRatio)
+    fmt.Printf("Reward: %v\n", feeHistory.Reward)
+}
+
+
+func waitForTransaction(client *ethclient.Client, tx *types.Transaction) *types.Receipt {
+    ctx := context.Background()
+    receipt, err := bind.WaitMined(ctx, client, tx)
+    if err != nil {
+        log.Fatalf("Failed to wait for transaction to be mined: %v", err)
+    }
+    return receipt
+}
+
+func testGetBlockTransactionCountByHash(client *ethclient.Client, blockHash common.Hash) {
+    txCount, err := client.TransactionCount(context.Background(), blockHash)
+    if err != nil {
+        log.Fatalf("Failed to get transaction count by block hash: %v", err)
+    }
+    fmt.Printf("Transaction count by block hash %s: %d\n", blockHash.Hex(), txCount)
+}
+
+func testGetBlockTransactionCountByNumber(client *ethclient.Client) {
+    txCount, err := client.PendingTransactionCount(context.Background())
+    if err != nil {
+        log.Fatalf("Failed to get transaction count by block number: %v", err)
+    }
+    fmt.Printf("Transaction count by block number: %d\n", txCount)
+}
+
+func testCodeAt(client *ethclient.Client, address common.Address) {
+    code, err := client.CodeAt(context.Background(), address, nil)
+    if err != nil {
+        log.Fatalf("Failed to get code at address: %v", err)
+    }
+    fmt.Printf("Code at address %s: %s\n", address.Hex(), hex.EncodeToString(code)) // Not a SmartContract so empty string expected
+}
+
+func testGetLogs(client *ethclient.Client, address common.Address, topics []common.Hash) {
+    query := ethereum.FilterQuery{
+        Addresses: []common.Address{address},
+        Topics:    [][]common.Hash{topics},
+    }
+
+    logs, err := client.FilterLogs(context.Background(), query)
+    if err != nil {
+        log.Fatalf("Failed to get logs: %v", err)
+    }
+
+    fmt.Printf("Logs for address %s:\n", address.Hex())
+    for _, vLog := range logs { // Not a SmartContract so empty result expected
+        fmt.Printf("Log Block Number: %d\n", vLog.BlockNumber)
+        fmt.Printf("Log Index: %d\n", vLog.Index)
+        fmt.Printf("Log Data: %s\n", hex.EncodeToString(vLog.Data))
+    }
+}
+
+func testStorageAt(client *ethclient.Client, address common.Address, slot string) {
+    slotHash := common.HexToHash(slot)
+    storageValue, err := client.StorageAt(context.Background(), address, slotHash, nil)
+    if err != nil {
+        log.Fatalf("Failed to get storage at address: %v", err)
+    }
+    fmt.Printf("Storage at address %s slot %s: %s\n", address.Hex(), slot, hex.EncodeToString(storageValue))
+}
+
+func testGetTransactionByBlockHashAndIndex(client *ethclient.Client, blockHash common.Hash, index uint) {
+    tx, err := client.TransactionInBlock(context.Background(), blockHash, index)
+    if err != nil {
+        log.Fatalf("Failed to get transaction by block hash and index: %v", err)
+    }
+    fmt.Printf("Transaction in block hash %s at index %d: %s\n", blockHash.Hex(), index, tx.Hash().Hex())
+}
+
+func testGetTransactionByBlockNumberAndIndex(client *ethclient.Client, blockHash common.Hash, index uint) {
+    tx, err := client.TransactionInBlock(context.Background(), blockHash, index)
+    if err != nil {
+        log.Fatalf("Failed to get transaction by block number and index: %v", err)
+    }
+    fmt.Printf("Transaction in block number at index %d: %s\n", index, tx.Hash().Hex())
+}
+
+func testGetTransactionByHash(client *ethclient.Client, txHash string) {
+    tx, _, err := client.TransactionByHash(context.Background(), common.HexToHash(txHash))
+    if err != nil {
+        log.Fatalf("Failed to get transaction by hash: %v", err)
+    }
+    fmt.Printf("Transaction by hash: %s\n", tx.Hash().Hex())
+}
+
+func testGetTransactionCount(client *ethclient.Client) {
+    count, err := client.PendingTransactionCount(context.Background())
+    if err != nil {
+        log.Fatalf("Failed to get transaction count: %v", err)
+    }
+    fmt.Printf("Transaction count for address %d\n", count)
+}
+
+func testGetTransactionReceipt(client *ethclient.Client, txHash string) {
+    receipt, err := client.TransactionReceipt(context.Background(), common.HexToHash(txHash))
+    if err != nil {
+        log.Fatalf("Failed to get transaction receipt: %v", err)
+    }
+    fmt.Printf("Transaction receipt for hash %s: %v\n", txHash, receipt)
+}
+
+func testSyncing(client *ethclient.Client) {
+    syncing, err := client.SyncProgress(context.Background())
+    if err != nil {
+        log.Fatalf("Failed to get syncing status: %v", err)
+    }
+    if syncing == nil {
+        fmt.Println("Not syncing")
+    } else {
+        fmt.Printf("Syncing: %+v\n", syncing)
+    }
+}
