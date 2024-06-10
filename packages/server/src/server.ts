3f337e0cbb64926a0494a44ec761a3a1aecc9cee
Victor Yanev <victor.yanev@limechain.tech>
2024-06-10 14:44:33 +0300

    fix: remove unused import
    Signed-off-by: Victor Yanev <victor.yanev@limechain.tech>

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -27,7 +27,6 @@ import path from 'path';
 import fs from 'fs';
 import { v4 as uuid } from 'uuid';
 import { formatRequestIdMessage } from './formatters';
-import _ from 'lodash';
 
 const mainLogger = pino({
   name: 'hedera-json-rpc-relay',
