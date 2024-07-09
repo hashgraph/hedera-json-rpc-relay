a934c3c44d0c9afc36d7471c7dbc63a1e1aaf136
Georgi Lazarov <glazarov95@gmail.com>
2024-07-09 15:54:28 +0300

    chore: update copyright (#2678)
    * chore: update copyrights
    
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>
    
    * fix: package.json in subgraph example
    
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>
    
    ---------
    
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- tools/solidity-coverage-example/test/PrngSystemContract.ts
+++ tools/solidity-coverage-example/test/PrngSystemContract.ts
@@ -2,7 +2,7 @@
  *
  * Hedera Smart Contracts
  *
- * Copyright (C) 2023 Hedera Hashgraph, LLC
+ * Copyright (C) 2022-2024 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
