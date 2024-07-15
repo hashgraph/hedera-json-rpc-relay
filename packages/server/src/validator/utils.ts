9ad0fb3e945833be99c171ddf7691e0e5d31c540
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2024-07-15 16:56:43 +0300

    Fixes failing tests after param is null check added
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- packages/server/src/validator/utils.ts
+++ packages/server/src/validator/utils.ts
@@ -13,11 +13,12 @@ export function validateParam(index: number | string, param: any, validation: an
   if (requiredIsMissing(param, validation.required)) {
     throw predefined.MISSING_REQUIRED_PARAMETER(index);
   }
+
   if (param === null) {
     throw predefined.INVALID_PARAMETER(index, `The value passed is not valid: ${param}.`);
   }
 
-  if (param != null && Array.isArray(paramType)) {
+  if (param !== undefined && Array.isArray(paramType)) {
     const results: any[] = [];
     for (const type of paramType) {
       const validator = Validator.TYPES[type];
@@ -30,7 +31,7 @@ export function validateParam(index: number | string, param: any, validation: an
     }
   }
 
-  if (param != null && !Array.isArray(paramType)) {
+  if (param !== undefined && !Array.isArray(paramType)) {
     const result = isArray ? paramType.test(index, param, validation.type[1]) : paramType.test(param);
     if (result === false) {
       throw predefined.INVALID_PARAMETER(index, `${paramType.error}, value: ${param}`);
