7c3af69e6ff1dd90dc4304dfca8627432e00007d
Victor Yanev <victor.yanev@limechain.tech>
2024-08-16 15:23:42 +0300

    fix: `eth_estimateGas` opts to fallback estimation when a contract reverts
    Signed-off-by: Victor Yanev <victor.yanev@limechain.tech>

--- packages/relay/src/lib/eth.ts
+++ packages/relay/src/lib/eth.ts
@@ -609,7 +609,7 @@ export class EthImpl implements Eth {
         `${requestIdPrefix} Error raised while fetching estimateGas from mirror-node: ${JSON.stringify(e)}`,
       );
       // in case of contract revert, we don't want to return a predefined gas but the actual error with the reason
-      if (e instanceof MirrorNodeClientError && e.isContractReverted()) {
+      if (this.estimateGasThrows && e instanceof MirrorNodeClientError && e.isContractReverted()) {
         return predefined.CONTRACT_REVERT(e.detail || e.message, e.data);
       }
       return this.predefinedGasForTransaction(transaction, requestIdPrefix, e);
