5053a7afb5ff660d6481b76eb6bc959a02f8e1f1
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2024-08-22 09:40:14 +0200

    feat: tests
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>

--- packages/relay/src/lib/services/ethService/ethGetCodeService/index.ts
+++ packages/relay/src/lib/services/ethService/ethGetCodeService/index.ts
@@ -9,19 +9,13 @@ export class EthGetCodeService {
   constructor(private logger: Logger) {}
 
   public async execute(address: string): Promise<string> {
-    try {
-      if (!this.isSupported(address)) {
-        return '';
-      }
-      const filePath = path.join(__dirname, 'config', `precompile.bytecode`);
-      const result = readFileSync(filePath.replace('/dist/', '/src/'), 'utf-8');
-      this.logger.trace(`Result for address ${address}: ${result}`);
-      return result;
-    } catch (e: any) {
-      this.logger.trace(`Error occurred for address ${address}`);
-      this.logger.trace(e.message);
+    if (!this.isSupported(address)) {
       return '';
     }
+    const filePath = path.join(__dirname, 'config', `precompile.bytecode`);
+    const result = readFileSync(filePath.replace('/dist/', '/src/'), 'utf-8');
+    this.logger.trace(`Result for address ${address}: ${result}`);
+    return result;
   }
 
   private isSupported(address: string) {
