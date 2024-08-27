d5568ab0ec10640004056ff03f2c5fab095ad06a
mateuszm-arianelabs <mateusz.marcinkowski@arianelabs.com>
2024-08-27 14:01:45 +0200

    chore: typo in readme, cleanup in code
    Signed-off-by: mateuszm-arianelabs <mateusz.marcinkowski@arianelabs.com>

--- tools/wagmi-example/src/components/Contract.tsx
+++ tools/wagmi-example/src/components/Contract.tsx
@@ -1,4 +1,24 @@
-import {useDeploy, useGetGreet, useWriteGreet,} from "../hooks/use-deploy.ts";
+/*-
+ *
+ * Hedera JSON RPC Relay - Wagmi Example
+ *
+ * Copyright (C) 2022-2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import {useDeploy, useReadGreet, useWriteGreet,} from "../hooks/use-deploy.ts";
 import {useState} from "react";
 import {useAccount} from "wagmi";
 
@@ -6,7 +26,7 @@ export function Contract() {
   const {status} = useAccount()
 
   const {deployContract, contractAddress, isDeployed} = useDeploy();
-  const {data, status: greetStatusView, refetch} = useGetGreet(contractAddress)
+  const {data, status: greetStatusView, refetch} = useReadGreet(contractAddress)
   const {status: setGreetStatus, setGreeting} = useWriteGreet(contractAddress, refetch)
 
   const [greetingInput, setGreetingInput] = useState("")
@@ -14,7 +34,7 @@ export function Contract() {
   return status === "connected" ? (
     <div>
       <h2>Deploy greeter contract</h2>
-      {status === 'connected' && !isDeployed ? (
+      {!isDeployed ? (
         <div>
           <button onClick={deployContract}>Deploy</button>
         </div>
@@ -25,13 +45,13 @@ export function Contract() {
       {isDeployed && (
         <div>
           <p>Current greet: {JSON.stringify(data)}</p>
-          <p>get greet status: {greetStatusView}</p>
+          <p>Get greet status: {greetStatusView}</p>
           <hr/>
-          <div style={{display: "flex"}}>
+          <div className="flex">
             <input type="text" value={greetingInput} onChange={(e) => setGreetingInput(e.target.value)}/>
             <button onClick={() => setGreeting(greetingInput)}>Call contract</button>
           </div>
-          <p>send transaction status: {setGreetStatus}</p>
+          <p>Send transaction status: {setGreetStatus}</p>
         </div>
       )}
     </div>
