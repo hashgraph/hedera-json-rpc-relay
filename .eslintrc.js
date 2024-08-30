af0718710c30620545d49b8e4b48cf1ed89c057f
Victor Yanev <victor.yanev@limechain.tech>
2024-08-30 13:57:48 +0300

    chore: fix .eslintrc.js
    Signed-off-by: Victor Yanev <victor.yanev@limechain.tech>

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,7 +47,11 @@ module.exports = {
     },
     "rules": {
         "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/semi": "off",
         "comma-dangle": [2, "only-multiline"],
-        "semi": [2, "always"],
+        "semi": [2, "always"]
     }
 };
