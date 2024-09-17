3f1e91bb109c83900e7e9357995aa23b920e26ce
Eric Badiere <ebadiere@gmail.com>
2024-09-17 12:47:24 -0600

    feat: Defined a barebones mainnet-light test suite. (#2971)
    * feat: Defined a barebones mainnet-light test suite.
    
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    
    * fix: Applied feedback
    
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    
    * fix: Applied feedback
    
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    
    ---------
    
    Signed-off-by: ebadiere <ebadiere@gmail.com>
    Signed-off-by: Eric Badiere <ebadiere@gmail.com>

--- packages/server/tests/acceptance/rpc_batch2.spec.ts
+++ packages/server/tests/acceptance/rpc_batch2.spec.ts
@@ -152,7 +152,7 @@ describe('@api-batch-2 RPC Server Acceptance Tests', function () {
   });
 
   describe('eth_estimateGas', async function () {
-    it('@release should execute "eth_estimateGas"', async function () {
+    it('@release-light, @release should execute "eth_estimateGas"', async function () {
       const res = await relay.call(RelayCalls.ETH_ENDPOINTS.ETH_ESTIMATE_GAS, [{}], requestId);
       expect(res).to.contain('0x');
       expect(res).to.not.be.equal('0x');
@@ -459,7 +459,7 @@ describe('@api-batch-2 RPC Server Acceptance Tests', function () {
       expect(res).to.eq('0x0');
     });
 
-    it('@release should execute "eth_getBalance" for contract', async function () {
+    it('@release-light, @release should execute "eth_getBalance" for contract', async function () {
       const res = await relay.call(
         RelayCalls.ETH_ENDPOINTS.ETH_GET_BALANCE,
         [getBalanceContractAddress, 'latest'],
