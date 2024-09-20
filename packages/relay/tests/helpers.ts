c0a604825bfd6e79968b3a881ee773ca0b5f1a8a
Logan Nguyen <logan.nguyen@swirldslabs.com>
2024-09-20 18:00:28 -0500

    feat: reverted logic reworked on estimateFileTransactionsFee
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- packages/relay/tests/helpers.ts
+++ packages/relay/tests/helpers.ts
@@ -983,7 +983,7 @@ export const estimateFileTransactionsFee = (
   callDataSize: number,
   fileChunkSize: number,
   currentNetworkExchangeRateInCents: number,
-) => {
+): number => {
   const fileCreateTransactions = 1;
   const fileCreateFeeInCents = constants.NETWORK_FEES_IN_CENTS.FILE_CREATE_PER_5_KB;
 
@@ -996,16 +996,14 @@ export const estimateFileTransactionsFee = (
     constants.NETWORK_FEES_IN_CENTS.FILE_APPEND_BASE_FEE +
     lastFileAppendChunkSize * constants.NETWORK_FEES_IN_CENTS.FILE_APPEND_RATE_PER_BYTE;
 
-  const esitmatedFileCreateTxFee = Math.round(
-    ((fileCreateTransactions * fileCreateFeeInCents) / currentNetworkExchangeRateInCents) *
-      constants.HBAR_TO_TINYBAR_COEF,
+  const totalTxFeeInCents =
+    fileCreateTransactions * fileCreateFeeInCents +
+    fileAppendFeeInCents * fileAppendTransactions +
+    lastFileAppendChunkFeeInCents;
+
+  const estimatedTxFee = Math.round(
+    (totalTxFeeInCents / currentNetworkExchangeRateInCents) * constants.HBAR_TO_TINYBAR_COEF,
   );
 
-  const esitmatedFileAppendTxFee = Math.round(
-    ((fileAppendTransactions * fileAppendFeeInCents + lastFileAppendChunkFeeInCents) /
-      currentNetworkExchangeRateInCents) *
-      constants.HBAR_TO_TINYBAR_COEF,
-  );
-
-  return { esitmatedFileCreateTxFee, esitmatedFileAppendTxFee };
+  return estimatedTxFee;
 };
