cd4dd471441cb5118ca3c1eff9c18facd4dbcbdd
Victor Yanev <victor.yanev@limechain.tech>
2024-09-27 12:20:48 +0300

    fix: precompileCalls.spec.ts
    Signed-off-by: Victor Yanev <victor.yanev@limechain.tech>

--- packages/server/tests/acceptance/htsPrecompile/precompileCalls.spec.ts
+++ packages/server/tests/acceptance/htsPrecompile/precompileCalls.spec.ts
@@ -473,7 +473,7 @@ describe('@precompile-calls Tests for eth_call with HTS', async function () {
         expect(customFees.fixedFees.length).to.eq(2);
 
         expect(customFees.fixedFees[0].amount).to.exist;
-        expect(customFees.fixedFees[0].amount.toString()).to.eq(Hbar.fromTinybars(1).toString());
+        expect(customFees.fixedFees[0].amount.toString()).to.eq('1');
         expect(customFees.fixedFees[0].tokenId).to.eq(ZERO_HEX);
         expect(customFees.fixedFees[0].feeCollector).to.exist;
         expect(customFees.fixedFees[0].feeCollector.toLowerCase()).to.eq(accounts[0].address.toLowerCase());
