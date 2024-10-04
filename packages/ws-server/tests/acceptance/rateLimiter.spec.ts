7153fe124cdec8b4f9b47fc177fd8a6492f61e92
nikolay <n.atanasow94@gmail.com>
2024-10-04 13:01:06 +0300

    chore: edit rate limiter test
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- packages/ws-server/tests/acceptance/rateLimiter.spec.ts
+++ packages/ws-server/tests/acceptance/rateLimiter.spec.ts
@@ -25,6 +25,7 @@ import relayConstants from '@hashgraph/json-rpc-relay/dist/lib/constants';
 import { AliasAccount } from '@hashgraph/json-rpc-server/tests/types/AliasAccount';
 import { IPRateLimitExceeded } from '@hashgraph/json-rpc-server/dist/koaJsonRpc/lib/RpcError';
 import { ConfigService } from '@hashgraph/json-rpc-config-service/dist/services';
+import { configServiceTestHelper } from '../../../config-service/tests/configServiceTestHelper';
 
 describe('@web-socket-ratelimiter Rate Limit Tests', async function () {
   const rateLimitTier2 = Number(ConfigService.get('TIER_2_RATE_LIMIT') || relayConstants.DEFAULT_RATE_LIMIT.TIER_2);
@@ -45,7 +46,12 @@ describe('@web-socket-ratelimiter Rate Limit Tests', async function () {
     },
   ];
 
+  before(async () => {
+    configServiceTestHelper.dynamicOverride('WS_BATCH_REQUESTS_ENABLED', true);
+  });
+
   after(async () => {
+    configServiceTestHelper.dynamicOverride('WS_BATCH_REQUESTS_ENABLED', false);
     // expect all the connections to the WS server to be closed after all
     if (global && global.socketServer) {
       expect(global.socketServer._connections).to.eq(0);
