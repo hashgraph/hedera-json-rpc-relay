3ee53752f0c65c46fb185b3fc5d0e5dc1ec8f2cb
nikolay <n.atanasow94@gmail.com>
2024-10-04 11:39:14 +0300

    chore: hide github token
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- packages/config-service/src/services/loggerService.ts
+++ packages/config-service/src/services/loggerService.ts
@@ -4,7 +4,8 @@ export class LoggerService {
   static maskUpEnv(envName, envValue) {
     if (
       envName == GlobalConfig.ENTRIES.OPERATOR_KEY_MAIN.envName ||
-      envName == GlobalConfig.ENTRIES.OPERATOR_KEY_ETH_SENDRAWTRANSACTION.envName
+      envName == GlobalConfig.ENTRIES.OPERATOR_KEY_ETH_SENDRAWTRANSACTION.envName ||
+      envName == GlobalConfig.ENTRIES.GITHUB_TOKEN
     ) {
       return `${envName} = **********${envValue.slice(-4)}`;
     }
