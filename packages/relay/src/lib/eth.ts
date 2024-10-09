38a3d72020705e84b675fc836162b3f849cf7ba3
ebadiere <ebadiere@gmail.com>
2024-10-09 16:50:43 -0600

    feat: Added consensus node information to the logging.
    Signed-off-by: ebadiere <ebadiere@gmail.com>

--- packages/relay/src/lib/eth.ts
+++ packages/relay/src/lib/eth.ts
@@ -1489,7 +1489,11 @@ export class EthImpl implements Eth {
   ): Promise<string | JsonRpcError> {
     this.logger.error(
       e,
-      `${requestDetails.formattedRequestId} Failed to successfully submit sendRawTransaction for transaction ${transaction}`,
+      `${
+        requestDetails.formattedRequestId
+      } Failed to successfully submit sendRawTransaction for transaction ${transaction} to node ${JSON.stringify(
+        this.hapiService.getMainClientInstance().network,
+      )}`,
     );
     if (e instanceof JsonRpcError) {
       return e;
@@ -1928,7 +1932,12 @@ export class EthImpl implements Eth {
         `Invalid contractCallResponse from consensus-node: ${JSON.stringify(contractCallResponse)}`,
       );
     } catch (e: any) {
-      this.logger.error(e, `${requestIdPrefix} Failed to successfully submit contractCallQuery`);
+      this.logger.error(
+        e,
+        `${requestIdPrefix} Failed to successfully submit contractCallQuery to consensus-node: ${JSON.stringify(
+          this.hapiService.getMainClientInstance().network,
+        )}`,
+      );
       if (e instanceof JsonRpcError) {
         return e;
       }
