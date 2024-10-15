24573c67350708f07fa096eb6ea3ef2afd9dc57c
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2024-10-15 11:47:12 +0300

    Removes setting of env from terminal command, adds it to test setup instead
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- packages/server/tests/acceptance/hbarLimiter.spec.ts
+++ packages/server/tests/acceptance/hbarLimiter.spec.ts
@@ -190,7 +190,7 @@ describe('@hbarlimiter HBAR Limiter Acceptance Tests', function () {
 
       before(async function () {
         // Restart the relay to reset the limits
-        //await global.restartLocalRelay();
+        await global.restartLocalRelay();
         await cacheService.clear(requestDetails);
         hbarSpendingPlanRepository = new HbarSpendingPlanRepository(cacheService, logger);
         ethAddressSpendingPlanRepository = new EthAddressHbarSpendingPlanRepository(cacheService, logger);
