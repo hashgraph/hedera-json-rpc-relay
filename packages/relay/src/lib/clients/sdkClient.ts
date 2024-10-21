d708f2063e7b12d95350d19c8dde4f8e8448a436
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2024-10-21 17:25:50 +0300

    Removes querying to mirrorNode
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- packages/relay/src/lib/clients/sdkClient.ts
+++ packages/relay/src/lib/clients/sdkClient.ts
@@ -69,9 +69,6 @@ import {
   ITransactionRecordMetric,
   RequestDetails,
 } from '../types';
-import MirrorNode from '@hashgraph/sdk/lib/MirrorNode';
-import { MirrorNodeClient } from './mirrorNodeClient';
-import { Registry } from 'prom-client';
 
 const _ = require('lodash');
 
@@ -117,8 +114,6 @@ export class SDKClient {
    */
   private readonly eventEmitter: EventEmitter;
 
-  private readonly mirrorNodeClient: MirrorNodeClient;
-
   /**
    * An instance of the HbarLimitService that tracks hbar expenses and limits.
    * @private
@@ -142,9 +137,6 @@ export class SDKClient {
     eventEmitter: EventEmitter,
     hbarLimitService: HbarLimitService,
   ) {
-    const registry = new Registry();
-    const restUrl = process.env.MIRROR_NODE_REST_URL || '';
-    this.mirrorNodeClient = new MirrorNodeClient(restUrl, logger, registry, cacheService);
     this.clientMain = clientMain;
 
     if (ConfigService.get('CONSENSUS_MAX_EXECUTION_TIME')) {
@@ -752,25 +744,10 @@ export class SDKClient {
       );
 
       if (!transactionResponse) {
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-        const transactionId = transaction.transactionId ? transaction.transactionId.toString() : false;
-        console.log('Transaction id', transactionId);
-        if (transactionId) {
-          try {
-            const transactionFromMirrorNode = await this.mirrorNodeClient.getTransactionById(
-              transactionId,
-              requestDetails,
-            );
-            this.logger.warn('Transaction found', transactionFromMirrorNode);
-          } catch (e) {
-            this.logger.warn('Transaction not found in mirror node', e);
-          }
-        }
         throw predefined.INTERNAL_ERROR(
-          `${requestDetails.formattedRequestId} TTransaction execution returns a null value: transactionId=${transaction.transactionId}, callerName=${callerName}, txConstructorName=${txConstructorName}`,
+          `${requestDetails.formattedRequestId} Transaction execution returns a null value: transactionId=${transaction.transactionId}, callerName=${callerName}, txConstructorName=${txConstructorName}`,
         );
       }
-
       return transactionResponse;
     } finally {
       if (transactionId?.length) {
