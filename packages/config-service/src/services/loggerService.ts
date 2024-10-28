dc49c9dfc6699fceac181636e6c07871fa2532f9
Victor Yanev <victor.yanev@limechain.tech>
2024-10-28 10:47:36 +0200

    Merge branch 'main' into 3096-Protect-preconfigured-spending-plans-from-being-deleted
    Signed-off-by: Victor Yanev <victor.yanev@limechain.tech>
    
    # Conflicts:
    #	packages/server/tests/acceptance/hbarLimiter.spec.ts

--- packages/config-service/src/services/loggerService.ts
+++ packages/config-service/src/services/loggerService.ts
@@ -28,6 +28,17 @@ export class LoggerService {
     GlobalConfig.ENTRIES.GH_ACCESS_TOKEN.envName,
   ];
 
+  public static readonly KNOWN_SECRET_PREFIXES = [
+    'ghp', // GitHub personal access tokens
+    'gho', // OAuth access tokens
+    'ghu', // GitHub user-to-server tokens
+    'ghs', // GitHub server-to-server tokens
+    'ghr', // refresh tokens
+  ];
+
+  public static readonly GITHUB_SECRET_PATTERN: RegExp =
+    /^(gh[pousr]_[a-zA-Z0-9]{36,251}|github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59})$/;
+
   /**
    * Hide sensitive information
    *
@@ -35,7 +46,11 @@ export class LoggerService {
    * @param envValue
    */
   static maskUpEnv(envName: string, envValue: string | undefined): string {
-    if (this.SENSITIVE_FIELDS.indexOf(envName) > -1) {
+    const isSensitiveField: boolean = this.SENSITIVE_FIELDS.indexOf(envName) > -1;
+    const isKnownSecret: boolean =
+      GlobalConfig.ENTRIES[envName].type === 'string' && !!this.GITHUB_SECRET_PATTERN.exec(envValue ?? '');
+
+    if (isSensitiveField || isKnownSecret) {
       return `${envName} = **********`;
     }
 
