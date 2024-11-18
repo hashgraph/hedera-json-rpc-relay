f604f05d6bf5cf04efb4ff70e14c18dae822ac3f
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2024-11-18 12:13:14 +0200

    adds new header rule and removes conflict leftover
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,13 +28,10 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "prettier",
     ],
-<<<<<<< HEAD
-    "plugins": ["simple-import-sort"],
-=======
-    "plugins":  [
+    "plugins": [
+        "simple-import-sort",
         "header"
     ],
->>>>>>> 497d7a52 (Adds check for license header)
     "overrides": [
         {
             "env": {
@@ -70,5 +67,26 @@ module.exports = {
         }],
         "simple-import-sort/imports": "error",
         "simple-import-sort/exports": "off",
+        "header/header": [2, "block", [
+            " -",
+            " *",
+            " * Hedera JSON RPC Relay",
+            " *",
+            " * Copyright (C) \\d{4}(-\\d{4})? Hedera Hashgraph, LLC",
+            " *",
+            " * Licensed under the Apache License, Version 2.0 (the \"License\");",
+            " * you may not use this file except in compliance with the License.",
+            " * You may obtain a copy of the License at",
+            " *",
+            " *      http://www.apache.org/licenses/LICENSE-2.0",
+            " *",
+            " * Unless required by applicable law or agreed to in writing, software",
+            " * distributed under the License is distributed on an \"AS IS\" BASIS,",
+            " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.",
+            " * See the License for the specific language governing permissions and",
+            " * limitations under the License.",
+            " *",
+            " "
+        ]]
     },
 };
