37377d9982f8440e7eee9c842e0322c5f63e3814
Mihail Mihov <mihail.mihov@limechain.tech>
2024-11-26 14:53:00 +0200

    ci: Add -Z flag to codecov cli
    Signed-off-by: Mihail Mihov <mihail.mihov@limechain.tech>

--- .github/scripts/codecov-upload.sh
+++ .github/scripts/codecov-upload.sh
@@ -4,7 +4,7 @@ curl -Os https://uploader.codecov.io/latest/linux/codecov && chmod +x codecov
 
 if [ -d "coverage" ]; then
   echo "Uploading coverage report for root"
-  ./codecov --token "${CODECOV_TOKEN}" --fail-on-error
+  ./codecov --token "${CODECOV_TOKEN}" --fail-on-error -Z
 else
   echo "No coverage report found for root"
 fi
@@ -12,7 +12,7 @@ fi
 for dir in packages/*; do
   if [ -d "${dir}/coverage" ]; then
     echo "Uploading coverage report for ${dir}"
-    ./codecov --dir "${dir}" --flags "$(basename "${dir}")" --token "${CODECOV_TOKEN}" --fail-on-error
+    ./codecov --dir "${dir}" --flags "$(basename "${dir}")" --token "${CODECOV_TOKEN}" --fail-on-error -Z
   else
     echo "No coverage report found for ${dir}"
   fi
