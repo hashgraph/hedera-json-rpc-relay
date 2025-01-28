9d8f20e93cfb2b083dccaa2b7dae5e8ed73d9b2a
Logan Nguyen <logan.nguyen@swirldslabs.com>
2025-01-28 10:16:59 -0600

    chore: updated a unique JsonRPC error code for TIMESTAMP_RANGE_TOO_LARGE
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- packages/relay/src/lib/errors/JsonRpcError.ts
+++ packages/relay/src/lib/errors/JsonRpcError.ts
@@ -135,7 +135,7 @@ export const predefined = {
     }),
   TIMESTAMP_RANGE_TOO_LARGE: (fromBlock: string, fromTimestamp: number, toBlock: string, toTimestamp: number) =>
     new JsonRpcError({
-      code: -32000,
+      code: -32004,
       message: `The provided fromBlock and toBlock contain timestamps that exceed the maximum allowed duration of 7 days (604800 seconds): fromBlock: ${fromBlock} (${fromTimestamp}), toBlock: ${toBlock} (${toTimestamp})`,
     }),
   REQUEST_BEYOND_HEAD_BLOCK: (requested: number, latest: number) =>
