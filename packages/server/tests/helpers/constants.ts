8a4afbbe97f6c827fd28824c67c33c2beeb23a8c
Simeon Nakov <simeon.nakov@limechain.tech>
2025-02-26 14:11:40 +0200

    feat: add support for trace_* rpc methods
    Signed-off-by: Simeon Nakov <simeon.nakov@limechain.tech>

--- packages/server/tests/helpers/constants.ts
+++ packages/server/tests/helpers/constants.ts
@@ -55,6 +55,7 @@ const ETH_ENDPOINTS = {
   ETH_GET_FILTER_CHANGES: 'eth_getFilterChanges',
   DEBUG_TRACE_TRANSACTION: 'debug_traceTransaction',
   ENGINE: ['engine_getPayloadV1', 'engine_newPayloadV1', 'engine_forkchoiceUpdatedV1', 'engine_exchangeCapabilities'],
+  TRACE: ['trace_block', 'trace_transaction', 'trace_call', 'trace_callMany'],
 };
 
 //Events
